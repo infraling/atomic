694f3ca026057e2664d97523251562b39ca6dfd1
sdruskat <stephan.druskat@uni-jena.de>
2013-10-29 21:35:08 +0100

    Minor correction

--- de.uni_jena.iaa.linktype.atomic.core/src/de/uni_jena/iaa/linktype/atomic/core/workspace/PickWorkspaceDialog.java
+++ de.uni_jena.iaa.linktype.atomic.core/src/de/uni_jena/iaa/linktype/atomic/core/workspace/PickWorkspaceDialog.java
@@ -368,7 +368,7 @@ public class PickWorkspaceDialog extends TitleAreaDialog {
 	                    fin.close();
 	                }
 	                if (fout != null) {
-	                    fin.close();
+	                    fout.close();
 	                }
 	            }
 	        }
