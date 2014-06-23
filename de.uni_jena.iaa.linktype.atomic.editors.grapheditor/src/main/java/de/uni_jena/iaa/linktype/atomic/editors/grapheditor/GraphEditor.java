87b10107eb5b8aed166a993eb9a709ece7a85621
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-06-23 13:21:53 +0200

    Implement URIResolver in editor class

--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/GraphEditor.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/GraphEditor.java
@@ -3,16 +3,30 @@
  */
 package de.uni_jena.iaa.linktype.atomic.editors.grapheditor;
 
+import java.io.File;
 import java.util.HashSet;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.gef.DefaultEditDomain;
 import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.editparts.ScalableFreeformRootEditPart;
 import org.eclipse.gef.palette.PaletteRoot;
 import org.eclipse.gef.ui.parts.GraphicalEditorWithFlyoutPalette;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.part.FileEditorInput;
 
+import de.hu_berlin.german.korpling.saltnpepper.salt.SaltFactory;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.SaltProject;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpus;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusGraph;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SDocument;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SToken;
 import de.uni_jena.iaa.linktype.atomic.editors.grapheditor.factories.AtomicEditPartFactory;
@@ -26,6 +40,9 @@ public class GraphEditor extends GraphicalEditorWithFlyoutPalette {
 	private boolean isModelGraph = false; // Used for instance-testing for setContents()
 	private HashSet<SToken> tokenMap; // For use as model for sub-graph editing
 	private SDocumentGraph graph; // For use as model for graph editing
+	private SaltProject project;
+	private URI projectURI;
+	private URI graphURI;
 
 	/**
 	 * 
@@ -44,6 +61,7 @@ public class GraphEditor extends GraphicalEditorWithFlyoutPalette {
 	protected void initializeGraphicalViewer() {
 		super.initializeGraphicalViewer();
 		if (isModelGraph) {
+			System.err.println(project);
 			getGraphicalViewer().setContents(graph);
 		}
 		else {
@@ -65,16 +83,121 @@ public class GraphEditor extends GraphicalEditorWithFlyoutPalette {
 	 */
 	@Override
 	public void doSave(IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-
+		graph.getSDocument().saveSDocumentGraph(graphURI);
+		// TODO Check if project also needs to be saved
+//		project.saveSaltProject(projectURI);
 	}
 	
 	@Override
 	protected void setInput(IEditorInput input) {
 		super.setInput(input);
-		System.err.println(input.getClass());
-		
-//		if (input.g)
+		if (input instanceof FileEditorInput) {
+			IFile file = ((FileEditorInput) input).getFile();
+			if (file.getName().equals(SaltFactory.FILE_SALT_PROJECT)) {
+				// TODO Handle this
+			}
+			else if (file.getName().endsWith(SaltFactory.FILE_ENDING_SALT) && !(file.getName().equals(SaltFactory.FILE_SALT_PROJECT))) {
+				GraphResolver graphResolver = new GraphResolver(file);
+				projectURI = graphResolver.getProjectURI();
+				graphURI = graphResolver.getGraphURI();
+				SaltProject saltProject = SaltFactory.eINSTANCE.createSaltProject();
+				saltProject.loadSCorpusStructure(projectURI);
+				for (SDocument document : saltProject.getSCorpusGraphs().get(0).getSDocuments()) {
+					if (document.getSDocumentGraphLocation() == (graphURI)) {
+						document.loadSDocumentGraph();
+						graph = document.getSDocumentGraph();
+						System.err.println("GRAPH SET TO " + graph);
+					}
+				}
+				isModelGraph = true;
+			}
+		}
+		else /*if (input instanceof AtomicSubGraphInput) */{ // TODO to be defined in Overview Editor
+			
+		}
+	}
+	/**
+	 * @author Stephan Druskat
+	 *
+	 */
+	public class GraphResolver {
+
+		private final Shell shell = Display.getCurrent().getActiveShell();
+		private IFile file;
+		private URI saltProjectURI, sDocumentGraphURI;
+		private SaltFactory sf = SaltFactory.eINSTANCE;
+
+		public GraphResolver(IFile file) {
+			this.file = file;
+			saltProjectURI = determineSaltProjectURI();
+			sDocumentGraphURI = determineSDocumentGraphURI();
+		}
+
+		/**
+		 * @param file
+		 * @return saltProject
+		 */
+		private URI determineSaltProjectURI() {
+			URI localSaltProjectURI;
+			if (file.getProject().getFile(SaltFactory.FILE_SALT_PROJECT).exists()) {
+				localSaltProjectURI = URI.createFileURI(new File(file.getProject().getFile(SaltFactory.FILE_SALT_PROJECT).getLocation().toString()).getAbsolutePath());
+			}
+			else { // I.e., graph is an "orphan" (not attached to a project)
+				localSaltProjectURI = URI.createFileURI(new File(file.getProject().getLocation().toString()).getAbsolutePath());
+				String documentName = file.getName().substring(0, file.getName().length() - 5);
+				String corpusName = file.getParent().getName();
+				SaltProject temporaryProject = sf.createSaltProject();
+				createSimpleCorpusStructure(temporaryProject, corpusName, documentName);
+				temporaryProject.saveSaltProject(localSaltProjectURI);
+				MessageDialog.openInformation(shell, "Salt project file created!", "The corpus document you are opening is an orphan, i.e., is not associated with an existing Salt project.\nTherefore, a new Salt project has been created, and the document has been attached to it.");
+				try {
+					file.getProject().refreshLocal(IProject.DEPTH_INFINITE, null);
+				} catch (CoreException e) {
+					MessageDialog.openError(shell, "Refresh Error!", "Auto-refreshing the Navigation View did not work. Please refresh manually by pressing F5 in the Navigation View, or \"Refresh\" in the context manu of the Navigation View.");
+					e.printStackTrace();
+				}
+			}
+			return localSaltProjectURI;
+		}
+
+		private URI determineSDocumentGraphURI() {
+			URI localSDocumentGraphURI = URI.createFileURI(new File(file.getLocation().toString()).getAbsolutePath());
+			if (saltProjectURI != null) {
+				SaltProject localSaltProject = sf.createSaltProject();
+				localSaltProject.loadSCorpusStructure(saltProjectURI);
+				if (localSaltProject.getSDocumentGraphLocations().containsValue(localSDocumentGraphURI)) {
+					return localSDocumentGraphURI;
+				}
+				else {
+					MessageDialog.openError(shell, "Graph Resolution Error", "No document graph with the URI " + localSDocumentGraphURI + " is registered in the project.");
+					return null;
+				}
+			}
+			return null;
+		}
+
+		private void createSimpleCorpusStructure(SaltProject saltProject, String corpusName, String documentName) {
+			saltProject.setSName("Generic Salt project for " + documentName);
+			SaltFactory sf = SaltFactory.eINSTANCE;
+			SCorpusGraph corpusGraph = sf.createSCorpusGraph();
+			saltProject.getSCorpusGraphs().add(corpusGraph);
+			SCorpus corpus = sf.createSCorpus();
+			corpus.setSName(corpusName);
+			corpusGraph.addSNode(corpus);
+			SDocument document = sf.createSDocument();
+			document.setSName(documentName);
+			SDocumentGraph protograph = sf.loadSDocumentGraph(URI.createFileURI(new File(file.getLocation().toString()).getAbsolutePath()));
+			document.setSDocumentGraph(protograph);
+			corpusGraph.addSDocument(corpus, document);
+		}
+
+		public URI getGraphURI() {
+			return sDocumentGraphURI;
+		}
+
+		public URI getProjectURI() {
+			return saltProjectURI;
+		}
 	}
 
 }
---
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/GraphEditorActionBarContributor.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.uni_jena.iaa.linktype.atomic.editors.grapheditor;
+
+import org.eclipse.ui.part.EditorActionBarContributor;
+
+/**
+ * @author Stephan Druskat
+ *
+ */
+public class GraphEditorActionBarContributor extends EditorActionBarContributor {
+
+	/**
+	 * 
+	 */
+	public GraphEditorActionBarContributor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
