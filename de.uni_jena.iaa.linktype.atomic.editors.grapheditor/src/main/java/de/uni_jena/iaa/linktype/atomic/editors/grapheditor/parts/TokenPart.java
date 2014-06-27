5c16b7da3b196f4a7b5735a7bfc7c8eff53178ff
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-06-27 12:52:23 +0200

    Add adapter to TokenPart

--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/parts/TokenPart.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/parts/TokenPart.java
@@ -3,9 +3,16 @@
  */
 package de.uni_jena.iaa.linktype.atomic.editors.grapheditor.parts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SToken;
@@ -17,6 +24,13 @@ import de.uni_jena.iaa.linktype.atomic.editors.grapheditor.util.PartUtils;
  *
  */
 public class TokenPart extends AbstractGraphicalEditPart {
+	
+	private TokenAdapter adapter;
+
+	public TokenPart() {
+		super();
+		setAdapter(new TokenAdapter());
+	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.gef.editparts.AbstractGraphicalEditPart#createFigure()
@@ -53,8 +67,65 @@ public class TokenPart extends AbstractGraphicalEditPart {
 
 	}
 	
+	@Override 
+	protected List<EObject> getModelChildren() {
+		List<EObject> childrenList = new ArrayList<EObject>();
+		childrenList.addAll(getModel().getSAnnotations());
+		return childrenList;
+	}
+	
 	public SToken getModel() {
 		return (SToken) super.getModel();
+	}
+
+	/**
+	 * @return the adapter
+	 */
+	public TokenAdapter getAdapter() {
+		return adapter;
+	}
+
+	/**
+	 * @param adapter the adapter to set
+	 */
+	public void setAdapter(TokenAdapter adapter) {
+		this.adapter = adapter;
+	}
+	
+	/**
+	 * @author Stephan Druskat
+	 *
+	 */
+	public class TokenAdapter extends EContentAdapter {
+
+		@Override public void notifyChanged(Notification n) {
+			refreshChildren();
+	    }
+	 
+		@Override public Notifier getTarget() {
+	    	return getModel();
+	    }
+	 
+	    @Override public boolean isAdapterForType(Object type) {
+	    	return type.equals(SToken.class);
+	    }
+
+	}
+	
+	@Override 
+	public void activate() {
+		if(!isActive()) {
+			getModel().eAdapters().add(getAdapter());
+	    }
+		super.activate();
+	}
+	 
+	@Override 
+	public void deactivate() {
+		if(isActive()) {
+			getModel().eAdapters().remove(getAdapter());
+		}
+		super.deactivate();
 	}
 
 }
