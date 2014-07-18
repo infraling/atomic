0742eb4da7ebc2fd960e276ea5d714a70e4271df
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-07-18 02:50:44 +0200

    Implement AnnotationAnnotateCommand with regex

--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/commands/AnnotationAnnotateCommand.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/commands/AnnotationAnnotateCommand.java
@@ -3,8 +3,9 @@
  */
 package de.uni_jena.iaa.linktype.atomic.editors.grapheditor.commands;
 
-import java.util.Arrays;
+import java.util.regex.Pattern;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.impl.NotificationImpl;
 import org.eclipse.emf.common.util.EList;
@@ -27,6 +28,9 @@ import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SAnnotation;
 public class AnnotationAnnotateCommand extends Command {
 	
 	private String annotationInput;
+	private static final String ANNOTATION_WITH_NAMESPACE_REGEX = "(?:\\\\:|[^:])+:{2}(?:\\\\:|[^:])+:(?:\\\\:|[^:])+";
+	private static final String ANNOTATION_WITHOUT_NAMESPACE_REGEX = "(?:\\\\:|[^:])+:(?:\\\\:|[^:])+";
+	private static final String ONE_OR_TWO_COLONS = "(?<!\\\\):{1,2}";
 	private SAnnotation model;
 	private LabelableElement modelParent;
 	private String[] annotationKeyValue;
@@ -45,7 +49,6 @@ public class AnnotationAnnotateCommand extends Command {
 	
 	@Override 
 	public void execute() {
-		// FIXME: Take into account optional namespace!
 		/* FIXME: Write Unit tests for different input possibilities:
 		 * a:
 		 * :a
@@ -62,25 +65,37 @@ public class AnnotationAnnotateCommand extends Command {
 		String key = null;
 		String value = null;
 		String namespace = null;
-		setAnnotationKeyValue(constructAnnotationKeyValue(getAnnotationInput()));
-
-		key = getAnnotationKeyValue()[0];
-		value = getAnnotationKeyValue()[1];
-		if (getAnnotationKeyValue()[2] != null) {
-			namespace = getAnnotationKeyValue()[2];
+		boolean isInputValid = checkInputValidity(getAnnotationInput());
+		if (isInputValid) {
+			String[] annotationFields = segmentInput(getAnnotationInput());
+			int l = annotationFields.length;
+			Assert.isLegal(l == 2 || l == 3, "The length of the annotation fields array should be 2 or 3 to hold key, value, and optionally namespace, but it is not.");
+			if (l == 2) {
+				key = annotationFields[0];
+				value = annotationFields[1];
+			}
+			else if (l == 3) {
+				namespace = annotationFields[0];
+				key = annotationFields[1];
+				value = annotationFields[2];
+			}
+			else {
+				MessageDialog.openError(Display.getCurrent().getActiveShell(), "Error", "The length of the annotation fields array should be 2 or 3 to hold key, value, and optionally namespace, but it is not. Please report this error!");
+				return;
+			}
 		}
-		boolean isAnnotationArrayValid = checkAnnotationArray(getAnnotationKeyValue());
-		if (!isAnnotationArrayValid) {
-			MessageDialog.openError(Display.getCurrent().getActiveShell(), "Annotation error", "The annotation format is not correct.\nPlease use [namespace]::[key]:[value], where neither key nor value may be empty, and namespace is optional.");
+		else {
+			MessageDialog.openError(Display.getCurrent().getActiveShell(), "Annotation error", "The annotation format is not correct.\nPlease use the following format\n\n[namespace]::[key]:[value]\n\nwhere neither key nor value may be empty, and namespace is optional.\n[namespace], [key] and [value] may contain any characters except the newline character. Additionally, colons used within either field must be escaped with a backslash: \"\\:\".");
 			return;
 		}
-		
+
+		// FIXME: Clean this up!
 		// Determine type of model parent & get its List of annotations
 		EList<SAnnotation> existingAnnotations = getExistingAnnotationsFromModelParent(modelParent);
 		if (existingAnnotations != null) {
 			boolean keyExistsInPreexistingAnnotations = false;
 			for (SAnnotation preExistingAnnotation : existingAnnotations) {
-				if (preExistingAnnotation.getSName().equalsIgnoreCase(key) && preExistingAnnotation != model) {
+				if (preExistingAnnotation.getName().equalsIgnoreCase(key) && preExistingAnnotation != model) {
 					if (preExistingAnnotation.getValueString().equalsIgnoreCase(value)) { // Exact duplicate annotation exists
 						MessageDialog.openError(Display.getCurrent().getActiveShell(), "Duplicate annotation!", "This exact annotation exists already for this annotatable element.\nThe process will be aborted.");
 						return;
@@ -136,32 +151,16 @@ public class AnnotationAnnotateCommand extends Command {
 		}
 	}
 
-	private boolean checkAnnotationArray(String[] annotationKeyValue2) {
-		// TODO Auto-generated method stub
-		return false;
+	private String[] segmentInput(String annotationInput) {
+		Pattern oneOrTwoColons = Pattern.compile(ONE_OR_TWO_COLONS);
+		return oneOrTwoColons.split(annotationInput);
 	}
 
-	private String[] constructAnnotationKeyValue(String input) {
-		String[] annotationArray = new String[3];
-		if (input.contains("::")) { // I.e., has namespace part
-			String[] nsKV = input.split(":{2}");
-			System.err.println(Arrays.toString(nsKV));
-			annotationArray[2] = nsKV[0];
-			String[] kV = nsKV[1].split(":");
-			System.err.println(Arrays.toString(kV));
-			annotationArray[0] = kV[0];
-			annotationArray[1] = kV[1];
-			System.err.println(Arrays.toString(annotationArray) + "\n");
+	private boolean checkInputValidity(String input) {
+		if (input.matches(ANNOTATION_WITHOUT_NAMESPACE_REGEX) || input.matches(ANNOTATION_WITH_NAMESPACE_REGEX)) {
+			return true;
 		}
-		else {
-			String[] kV = input.split(":");
-			annotationArray[0] = kV[0];
-			annotationArray[1] = kV[1];
-			if (model.getNamespace() != null) {
-				annotationArray[2] = model.getNamespace();
-			}
-		}
-		return annotationArray;
+		return false;
 	}
 
 	private EList<SAnnotation> getExistingAnnotationsFromModelParent(LabelableElement modelParent) {
