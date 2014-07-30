68be737fedc90c6ff11e58896aead31f04d0cf86
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-07-31 01:39:07 +0200

    Fix StructuredNodeAnnotationCommand and respective EditPolicy to take into account optional namespace values

--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/commands/StructuredNodeAnnotateCommand.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/commands/StructuredNodeAnnotateCommand.java
@@ -7,10 +7,9 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.gef.commands.Command;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
 
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SStructuredNode;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SAnnotation;
@@ -21,57 +20,40 @@ import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SAnnotation;
  */
 public class StructuredNodeAnnotateCommand extends Command {
 	
-	private String annotationInput;
 	private SStructuredNode model;
-	private TreeMap<String, String> annotations = new TreeMap<String, String>();
+	private TreeMap<String, Pair<String, String>> annotations = new TreeMap<String, Pair<String, String>>();
 	
 	@Override 
 	public void execute() {
+		// Remove all annotations; FIXME: Save them in TreeMap<String, Pair<String,String>> oldAnnotations for undo
 		for (Iterator<SAnnotation> iterator = model.getSAnnotations().iterator(); iterator.hasNext();) {
 			SAnnotation anno = (SAnnotation) iterator.next();
 			String name = anno.getName();
 			model.removeLabel(name);
 			Assert.isTrue(!(model.getSAnnotations().contains(name))); // FIXME: Refactor to unit test method
 		}
-		
-		// TODO Refactor to consider optional namespace
-		String lineSeparator = System.getProperty("line.separator");
-		String[] keyValuePairs = annotationInput.split(lineSeparator);
-		for (int i = 0; i < keyValuePairs.length; i++) {
-			String[] keyValuePair = keyValuePairs[i].split(":");
-			String annoKey = keyValuePair[0];
-			String annoValue = keyValuePair[1];
-			String previousValue = annotations.put(annoKey, annoValue);
-			if (previousValue != null) {
-				boolean shouldAnnoChange = MessageDialog.openConfirm(Display.getCurrent().getActiveShell(), "Duplicate annotation found!", "Duplicate annotation!\n"
-						+ "Should the previous value of " + annoKey + " (" + previousValue + ") be overwritten with "
-						+ "the value " + annoValue + "?");
-				if (shouldAnnoChange) {
-					// Do nothing, key-value pair is already in TreeMap
-				}
-				else {
-					annotations.put(annoKey, previousValue);
-				}
-			}
-		}
-		for (Entry<String, String> anno : annotations.entrySet()) {
-			model.createSAnnotation(null, anno.getKey(), anno.getValue());
+		// Create annotations from input
+		for (Entry<String, Pair<String, String>> anno : getAnnotations().entrySet()) {
+			model.createSAnnotation(anno.getValue().getLeft(), anno.getKey(), anno.getValue().getRight());
 		}
 	}
 			 
-	//@Override 
-	//public void undo() {
-	//	if (model.getSAnnotations() == null)
-	//		//model.createSAnnotation(null, "ATOMIC__PRIMARY_ANNOTATION", oldAnnotation);
-	//	model.getSAnnotation("ATOMIC__PRIMARY_ANNOTATION").setSValue(oldAnnotation);
-	//}
-			   
-	public void setNewAnnotation(String newAnnotation) {
-		this.annotationInput = newAnnotation;
-	}
-			   
 	public void setModel(SStructuredNode model) {
 		this.model = model;
+	}
+
+	/**
+	 * @return the annotations
+	 */
+	public TreeMap<String,Pair<String,String>> getAnnotations() {
+		return annotations;
+	}
+
+	/**
+	 * @param annotations the annotations to set
+	 */
+	public void setAnnotations(TreeMap<String, Pair<String, String>> annotations) {
+		this.annotations = annotations;
 	}
 
 }
--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/util/AnnotationUtils.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/util/AnnotationUtils.java
@@ -5,12 +5,6 @@ package de.uni_jena.iaa.linktype.atomic.editors.grapheditor.util;
 
 import java.util.regex.Pattern;
 
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
-
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SAnnotation;
-
 /**
  * @author Stephan Druskat
  *
@@ -20,8 +14,6 @@ public class AnnotationUtils {
 	private static final String ANNOTATION_WITH_NAMESPACE_REGEX = "(?:\\\\:|[^:])+:{2}(?:\\\\:|[^:])+:(?:\\\\:|[^:])+";
 	private static final String ANNOTATION_WITHOUT_NAMESPACE_REGEX = "(?:\\\\:|[^:])+:(?:\\\\:|[^:])+";
 	private static final String ONE_OR_TWO_COLONS = "(?<!\\\\):{1,2}";
-	private static final String XXO_MESSAGE = "An annotation with the key %s and the value %s already exists (in the namespace %s)!\nShould an additional annotation with this key and this value be created in the namespace %s nevertheless?";
-	private static final String XOX_MESSAGE = "An annotation with the key %s already exists in the namespace %s!\nShould the current value of this annotation (%s) be overwritten with %s?";
 
 	public static boolean checkInputValidity(String input) {
 		if (input.matches(ANNOTATION_WITHOUT_NAMESPACE_REGEX) || input.matches(ANNOTATION_WITH_NAMESPACE_REGEX)) {
@@ -33,11 +25,6 @@ public class AnnotationUtils {
 	public static String[] segmentInput(String annotationInput) {
 		Pattern oneOrTwoColons = Pattern.compile(ONE_OR_TWO_COLONS);
 		return oneOrTwoColons.split(annotationInput);
-	}
-
-	public static  boolean checkAnnotationValuesAgainstExisting(SAnnotation model, EList<SAnnotation> existingAnnotations, String namespace, String key, String value) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
