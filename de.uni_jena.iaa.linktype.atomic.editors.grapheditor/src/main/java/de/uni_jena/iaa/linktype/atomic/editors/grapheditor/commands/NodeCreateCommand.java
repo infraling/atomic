8d6ba00ea6d974d998e4264ccb8cea4d8dce99ca
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-09-24 01:05:54 +0200

    Implement on-the-fly creation of nodes + edges over selected nodes and fix display of duplicate ids (e.g., structure2 & structure 2_49)

--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/commands/NodeCreateCommand.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/commands/NodeCreateCommand.java
@@ -3,12 +3,23 @@
  */
 package de.uni_jena.iaa.linktype.atomic.editors.grapheditor.commands;
 
+import java.util.List;
+
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.impl.NotificationImpl;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
 
+import de.hu_berlin.german.korpling.saltnpepper.salt.SaltFactory;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SSpan;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SStructure;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SStructuredNode;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SToken;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SDATATYPE;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SNode;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SRelation;
 
 /**
  * @author Stephan Druskat
@@ -19,13 +30,49 @@ public class NodeCreateCommand extends Command {
 	private SDocumentGraph graph;
 	private SStructuredNode model;
 	private Point location;
+	private List<EditPart> selectedEditParts;
 
 	@Override
 	public void execute() {
 		getModel().setGraph(getGraph());
 		getModel().createSProcessingAnnotation("ATOMIC", "GRAPHEDITOR_COORDS", new int[]{getLocation().x, getLocation().y, 1}, SDATATYPE.SOBJECT);
+		if (!getSelectedEditParts().isEmpty()) {
+			createRelations();
+		}
 	}
 	
+	private void createRelations() {
+		if (getModel() instanceof SStructure) {
+			for (EditPart editPart : getSelectedEditParts()) {
+				Object ePModel = editPart.getModel();
+				SRelation relation = null;
+				if (ePModel instanceof SStructure || ePModel instanceof SToken || ePModel instanceof SSpan) {
+					relation = SaltFactory.eINSTANCE.createSDominanceRelation();
+					relation.setSSource(getModel());
+					relation.setSTarget((SNode) ePModel);
+					relation.setGraph(getGraph());
+					getModel().eNotify(new NotificationImpl(Notification.ADD, null, relation));
+					((SNode) ePModel).eNotify(new NotificationImpl(Notification.ADD, null, relation));
+				}
+			}
+		}
+		else if (getModel() instanceof SSpan) {
+			for (EditPart editPart : getSelectedEditParts()) {
+				Object ePModel = editPart.getModel();
+				SRelation relation = null;
+				if (ePModel instanceof SToken) {
+					relation = SaltFactory.eINSTANCE.createSSpanningRelation();
+					relation.setSSource(getModel());
+					relation.setSTarget((SNode) ePModel);
+					relation.setGraph(getGraph());
+					getModel().eNotify(new NotificationImpl(Notification.ADD, null, relation));
+					((SNode) ePModel).eNotify(new NotificationImpl(Notification.ADD, null, relation));
+				}
+			}
+		}
+		
+	}
+
 	@Override
 	public void undo() {
 		getModel().setGraph(null);
@@ -62,6 +109,17 @@ public class NodeCreateCommand extends Command {
 	 */
 	private Point getLocation() {
 		return location;
+	}
+
+	public void setSelectedEditParts(List<EditPart> selectedEditParts) {
+		this.selectedEditParts = selectedEditParts;
+	}
+
+	/**
+	 * @return the selectedEditParts
+	 */
+	private List<EditPart> getSelectedEditParts() {
+		return selectedEditParts;
 	}
 
 }
