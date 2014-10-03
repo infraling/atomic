6ea4eb2b479c30e2141416e607fd00ce56003393
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-10-03 17:33:39 +0200

    Add icons to graph editor palette

--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/Activator.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/Activator.java
@@ -1,6 +1,12 @@
 package de.uni_jena.iaa.linktype.atomic.editors.grapheditor;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -10,7 +16,16 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "de.uni_jena.iaa.linktype.atomic.editors.grapheditor"; //$NON-NLS-1$
+	public static final String iconsPath = "src/main/resources/icons/";
 
+	public static final String SPANNINGRELATION_ICON = "SPANNINGRELATION_ICON";
+	public static final String DOMINANCERELATION_ICON = "DOMINANCERELATION_ICON";
+	public static final String POINTINGRELATION_ICON = "POINTINGRELATION_ICON";
+	public static final String ORDERRELATION_ICON = "ORDERRELATION_ICON";
+	public static final String STRUCTURE_ICON = "STRUCTURE_ICON";
+	public static final String SPAN_ICON = "SPAN_ICON";
+
+	
 	// The shared instance
 	private static Activator plugin;
 	
@@ -46,5 +61,25 @@ public class Activator extends AbstractUIPlugin {
 	public static Activator getDefault() {
 		return plugin;
 	}
+	
+	@Override
+	protected void initializeImageRegistry(ImageRegistry registry) {
+		super.initializeImageRegistry(registry);
+        Bundle bundle = Platform.getBundle(PLUGIN_ID);
+
+        ImageDescriptor structure = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path(iconsPath + "structure.gif"), null));
+        ImageDescriptor span = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path(iconsPath + "span.gif"), null));
+        ImageDescriptor domRel = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path(iconsPath + "dominancerelation.gif"), null));
+        ImageDescriptor spanRel = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path(iconsPath + "spanningrelation.gif"), null));
+        ImageDescriptor pointRel = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path(iconsPath + "pointingrelation.gif"), null));
+        ImageDescriptor orderRel = ImageDescriptor.createFromURL(FileLocator.find(bundle, new Path(iconsPath + "orderrelation.gif"), null));
+        registry.put(STRUCTURE_ICON, structure);
+        registry.put(SPAN_ICON, span);
+        registry.put(DOMINANCERELATION_ICON, domRel);
+        registry.put(SPANNINGRELATION_ICON, spanRel);
+        registry.put(POINTINGRELATION_ICON, pointRel);
+        registry.put(ORDERRELATION_ICON, orderRel);
+
+    }
 
 }
--- de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/factories/GraphEditorPaletteFactory.java
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/factories/GraphEditorPaletteFactory.java
@@ -3,6 +3,7 @@
  */
 package de.uni_jena.iaa.linktype.atomic.editors.grapheditor.factories;
 
+import de.uni_jena.iaa.linktype.atomic.editors.grapheditor.Activator;
 import de.uni_jena.iaa.linktype.atomic.editors.grapheditor.factories.SaltElementsFactory.ElementType;
 
 import org.eclipse.gef.palette.CombinedTemplateCreationEntry;
@@ -16,6 +17,9 @@ import org.eclipse.gef.palette.PaletteToolbar;
 import org.eclipse.gef.palette.PanningSelectionToolEntry;
 import org.eclipse.gef.palette.ToolEntry;
 import org.eclipse.gef.tools.ConnectionCreationTool;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import de.uni_jena.iaa.linktype.atomic.editors.grapheditor.util.CreationAndDirectEditTool;
 
@@ -24,8 +28,25 @@ import de.uni_jena.iaa.linktype.atomic.editors.grapheditor.util.CreationAndDirec
  *
  */
 public class GraphEditorPaletteFactory {
+	
+	private static ImageDescriptor SPANNINGRELATION_DESCRIPTOR, DOMINANCERELATION_DESCRIPTOR, POINTINGRELATION_DESCRIPTOR, ORDERRELATION_DESCRIPTOR, STRUCTURE_DESCRIPTOR, SPAN_DESCRIPTOR;
+
+	/**
+	 * 
+	 */
+	private static void getImages() {
+		AbstractUIPlugin plugin = Activator.getDefault();
+		ImageRegistry imageRegistry = plugin.getImageRegistry();
+		SPANNINGRELATION_DESCRIPTOR = ImageDescriptor.createFromImage(imageRegistry.get(Activator.SPANNINGRELATION_ICON));
+		DOMINANCERELATION_DESCRIPTOR = ImageDescriptor.createFromImage(imageRegistry.get(Activator.DOMINANCERELATION_ICON));
+		POINTINGRELATION_DESCRIPTOR = ImageDescriptor.createFromImage(imageRegistry.get(Activator.POINTINGRELATION_ICON));
+		ORDERRELATION_DESCRIPTOR = ImageDescriptor.createFromImage(imageRegistry.get(Activator.ORDERRELATION_ICON));
+		STRUCTURE_DESCRIPTOR = ImageDescriptor.createFromImage(imageRegistry.get(Activator.STRUCTURE_ICON));
+		SPAN_DESCRIPTOR = ImageDescriptor.createFromImage(imageRegistry.get(Activator.SPAN_ICON));
+	}
 
 	public static PaletteRoot createPalette() {
+		getImages();
 		PaletteRoot palette = new PaletteRoot();
 		palette.add(createToolsGroup(palette));
 		palette.add(createNodesDrawer());
@@ -43,25 +64,25 @@ public class GraphEditorPaletteFactory {
 	}
 
 	private static PaletteEntry createOrderRelationTool() {
-		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Order relation", "Create a new order relation", new SaltElementsFactory(ElementType.ORDER_RELATION), null, null);
+		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Order relation", "Create a new order relation", new SaltElementsFactory(ElementType.ORDER_RELATION), ORDERRELATION_DESCRIPTOR, ORDERRELATION_DESCRIPTOR);
 		entry.setToolClass(ConnectionCreationTool.class);
 		return entry;
 	}
 
 	private static PaletteEntry createPointingRelationTool() {
-		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Pointing relation", "Create a new pointing relation", new SaltElementsFactory(ElementType.POINTING_RELATION), null, null);
+		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Pointing relation", "Create a new pointing relation", new SaltElementsFactory(ElementType.POINTING_RELATION), POINTINGRELATION_DESCRIPTOR, POINTINGRELATION_DESCRIPTOR);
 		entry.setToolClass(ConnectionCreationTool.class);
 		return entry;
 	}
 
 	private static PaletteEntry createSpanningRelationTool() {
-		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Spanning relation", "Create a new spanning relation", new SaltElementsFactory(ElementType.SPANNING_RELATION), null, null);
+		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Spanning relation", "Create a new spanning relation", new SaltElementsFactory(ElementType.SPANNING_RELATION), SPANNINGRELATION_DESCRIPTOR, SPANNINGRELATION_DESCRIPTOR);
 		entry.setToolClass(ConnectionCreationTool.class);
 		return entry;
 	}
 
 	private static PaletteEntry createDominanceRelationTool() {
-		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Dominance relation", "Create a new dominance relation", new SaltElementsFactory(ElementType.DOMINANCE_RELATION), null, null);
+		ConnectionCreationToolEntry entry = new ConnectionCreationToolEntry("Dominance relation", "Create a new dominance relation", new SaltElementsFactory(ElementType.DOMINANCE_RELATION), DOMINANCERELATION_DESCRIPTOR, DOMINANCERELATION_DESCRIPTOR);
 		entry.setToolClass(ConnectionCreationTool.class);
 		return entry;
 	}
@@ -74,12 +95,12 @@ public class GraphEditorPaletteFactory {
 	}
 
 	private static PaletteEntry createSpanTool() {
-		CreationToolEntry entry = new CombinedTemplateCreationEntry("Span", "Create a new Span", new SaltElementsFactory(ElementType.SPAN), null, null);//NODE_ICON, NODE_ICON);
+		CreationToolEntry entry = new CombinedTemplateCreationEntry("Span", "Create a new Span", new SaltElementsFactory(ElementType.SPAN), SPAN_DESCRIPTOR, SPAN_DESCRIPTOR);
 		entry.setToolClass(CreationAndDirectEditTool.class);
 		return entry;	}
 
 	private static PaletteEntry createStructureTool() {
-		CreationToolEntry entry = new CombinedTemplateCreationEntry("Structure", "Create a new Structure", new SaltElementsFactory(ElementType.STRUCTURE), null, null);//NODE_ICON, NODE_ICON);
+		CreationToolEntry entry = new CombinedTemplateCreationEntry("Structure", "Create a new Structure", new SaltElementsFactory(ElementType.STRUCTURE), STRUCTURE_DESCRIPTOR, STRUCTURE_DESCRIPTOR);
 		entry.setToolClass(CreationAndDirectEditTool.class);
 		return entry;	
 	}
