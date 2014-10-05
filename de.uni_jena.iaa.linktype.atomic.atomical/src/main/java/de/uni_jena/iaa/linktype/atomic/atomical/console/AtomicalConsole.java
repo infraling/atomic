0b9a1b9c3bfc5b828f9c742c926f0c36e382bfc2
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-10-05 19:03:10 +0200

    Add ConsoleFactory and change command to call it

---
+++ de.uni_jena.iaa.linktype.atomic.atomical/src/main/java/de/uni_jena/iaa/linktype/atomic/atomical/console/AtomicalConsole.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.uni_jena.iaa.linktype.atomic.atomical.console;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.console.IOConsole;
+
+/**
+ * @author Stephan Druskat
+ *
+ */
+public class AtomicalConsole extends IOConsole {
+
+	public AtomicalConsole(String name, ImageDescriptor imageDescriptor) {
+		super(name, imageDescriptor);
+	}
+
+}
---
+++ de.uni_jena.iaa.linktype.atomic.atomical/src/main/java/de/uni_jena/iaa/linktype/atomic/atomical/console/AtomicalConsoleFactory.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package de.uni_jena.iaa.linktype.atomic.atomical.console;
+
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleFactory;
+import org.eclipse.ui.console.IConsoleManager;
+
+/**
+ * @author Stephan Druskat
+ * Creates and opens an instance of AtomicalConsole in the ConsoleView
+ * 
+ */
+public class AtomicalConsoleFactory implements IConsoleFactory {
+
+	private static AtomicalConsole _console;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.console.IConsoleFactory#openConsole()
+	 */
+	@Override
+	public void openConsole() {
+		AtomicalConsole console = getConsole();
+		if (console != null) {
+			IConsoleManager manager = ConsolePlugin.getDefault().getConsoleManager();
+			IConsole[] existing = manager.getConsoles();
+			boolean exists = false;
+			// Check if instances of this console already exist
+			for (int i = 0; i < existing.length; i++) {
+				if (console == existing[i])
+					exists = true;
+			}
+			if (!exists)
+				manager.addConsoles(new IConsole[] { console });
+			manager.showConsoleView(console);
+		}
+	}
+
+	private static AtomicalConsole getConsole() {
+		if (_console == null) {
+			_console = new AtomicalConsole("AtomicAL Console", null);
+		}
+		return _console;
+	}
+
+}
