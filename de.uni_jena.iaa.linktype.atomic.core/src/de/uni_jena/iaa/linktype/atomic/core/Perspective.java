8d1dceb5ee844a241b9879ca84c90af4a3820961
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-10-24 02:00:47 +0200

    Disable opening the Console View / the Show View View via key shortcuts
    - Re-bind M3+M2+Q Q and M3+M2+Q Q to exit command.

--- de.uni_jena.iaa.linktype.atomic.core/src/de/uni_jena/iaa/linktype/atomic/core/Perspective.java
+++ de.uni_jena.iaa.linktype.atomic.core/src/de/uni_jena/iaa/linktype/atomic/core/Perspective.java
@@ -18,9 +18,16 @@ package de.uni_jena.iaa.linktype.atomic.core;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.contexts.IContextService;
 
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			   public void run() {
+			    ((IContextService) PlatformUI.getWorkbench().getService(IContextService.class)).activateContext("de.uni_jena.iaa.linktype.atomic.core.context");
+			   }
+			});
 	}
 }
