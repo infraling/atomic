af0cdfa2f8c05aa70ff778a57c2e630c8eb1ecd3
Stephan Druskat <stephan.druskat@uni-jena.de>
2014-11-12 11:34:54 +0100

    Tag for version 0.1.7

--- replace_files.sh
+++ replace_files.sh
@@ -6,24 +6,10 @@
 	cd ..
 	cd de.uni_jena.iaa.linktype.atomic.repository/target/products/
 for file in ./*.zip; do
-  echo "Working on" ${file##*/} "now."
+  echo "Replacing relANNIS and SaltXML plugins in" ${file##*/} "."
   zip -r ${file##*/} ./plugins/
-  #zip -r ${file##*/} test/
-  #mv ${file##*/} unzip/
 done
+rm -rf plugins/
 )
+echo "Finished!"
 exit 0
-
-
-
-
-
-
-#You can do the following, when your current directory is parent_directory:
-
-#for d in [0-9][0-9][0-9]
-#do
-#    ( cd $d && your-command-here )
-#done
-
-#The ( and ) create a subshell, so the current directory isn't changed in the main script.
