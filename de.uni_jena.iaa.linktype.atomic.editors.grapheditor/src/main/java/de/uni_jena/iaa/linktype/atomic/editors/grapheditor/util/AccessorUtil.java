b285bac772d6492b20a0d1afa9ad7ddc3a831bc3
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-01-23 14:47:54 +0100

    Add class AccessorUtil with static methods to easily access project properties

---
+++ de.uni_jena.iaa.linktype.atomic.editors.grapheditor/src/main/java/de/uni_jena/iaa/linktype/atomic/editors/grapheditor/util/AccessorUtil.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package de.uni_jena.iaa.linktype.atomic.editors.grapheditor.util;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Stephan Druskat
+ *
+ */
+public class AccessorUtil {
+	
+	private static final Logger log = LoggerFactory.getLogger(AccessorUtil.class);
+
+	/**
+	 * @param string
+	 * @return
+	 */
+	public static String getReservedKeysProperty(String key) {
+		try {
+			return ((FileEditorInput) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor().getEditorInput()).getFile().getProject().getPersistentProperty(new QualifiedName("de.uni_jena.iaa.linktype.atomic.core", key));
+		} catch (CoreException e) {
+			log.error("Core exception!", e);
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
