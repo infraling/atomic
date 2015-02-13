f5c22e55dcd105d8a5802e7e7ec3e5d2ecb96474
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-02-13 11:31:05 +0100

    Correct ordering of STokens

--- de.uni_jena.iaa.linktype.atomic.views.sentenceview/src/main/java/de/uni_jena/iaa/linktype/atomic/views/sentenceview/SentenceView.java
+++ de.uni_jena.iaa.linktype.atomic.views.sentenceview/src/main/java/de/uni_jena/iaa/linktype/atomic/views/sentenceview/SentenceView.java
@@ -37,7 +37,6 @@ import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.ViewPart;
 
 import de.hu_berlin.german.korpling.saltnpepper.salt.graph.Edge;
-import de.hu_berlin.german.korpling.saltnpepper.salt.graph.Node;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SSpan;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.STYPE_NAME;
@@ -100,7 +99,6 @@ public class SentenceView extends ViewPart implements ISelectionProvider {
 	protected String retrieveSentenceFromSpan(SSpan span) {
 		EList<SToken> overlappedTokens = span.getSDocumentGraph().getOverlappedSTokens(span, new BasicEList<STYPE_NAME>(Arrays.asList(STYPE_NAME.SSPANNING_RELATION)));
 		EList<SToken> sortedTokens = span.getSDocumentGraph().getSortedSTokenByText(overlappedTokens);
-		// FIXME: Is this safe for order of spans?
 		int sentenceIndex = span.getSDocumentGraph().getSLayer(ModelRegistry.SENTENCE_LAYER_SID).getAllIncludedNodes().indexOf(span) + 1;
 		String sentence = "";
 		for (int i = 0; i < sortedTokens.size(); i++) {
