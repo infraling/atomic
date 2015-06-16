715bcf5283049eac51383f825df5b5a60e2d7377
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-06-16 17:45:38 +0200

    Add plugin to feature

---
+++ de.uni_jena.iaa.linktype.atomic.views.linkedsentences/src/de/uni_jena/iaa/linktype/atomic/views/linkedsentences/LinkedSentencesView.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package de.uni_jena.iaa.linktype.atomic.views.linkedsentences;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * @author Stephan Druskat
+ *
+ */
+public class LinkedSentencesView extends ViewPart {
+
+	/**
+	 * 
+	 */
+	public LinkedSentencesView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+	 */
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
