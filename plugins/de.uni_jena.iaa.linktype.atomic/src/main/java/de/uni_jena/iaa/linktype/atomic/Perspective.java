7aaa3b886c54f6440f4f0ae4b4dce3ced6365487
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-08-27 01:35:27 +0200

    Create basic 3.x RCP application plugin

---
+++ plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/Perspective.java
@@ -0,0 +1,10 @@
+package de.uni_jena.iaa.linktype.atomic;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
