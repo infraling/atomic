f969813e53c773bfd57f6b5c5e24dc7f59972e9a
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-08-26 16:00:20 +0200

    Add log messages to ExitHandler

--- plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/handlers/ExitHandler.java
+++ plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/handlers/ExitHandler.java
@@ -3,17 +3,29 @@
  */
 package de.uni_jena.iaa.linktype.atomic.handlers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 
 public class ExitHandler {
+	
+	private static Logger log = LogManager.getLogger(ExitHandler.class);
+
 	/**
 	 * Handles exiting the Atomic workbench.
 	 * @param workbench The current workbench
 	 */
 	@Execute
 	public void execute(IWorkbench workbench) {
-		workbench.close();
+		log.trace("Close Atomic workbench.");
+		boolean shutdownSuccess = workbench.close();
+		if (!shutdownSuccess) {
+			log.error("Shutdown of Atomic workbench did not complete successfully.");
+		}
+		else {
+			log.trace("Atomic shut down successfully.");
+		}
 	}
 		
 }
