b087c296da33f02df62897c08f438ab7e46ea986
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-09-02 11:28:51 +0200

    Change log field template and re-insert

--- plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/Atomic.java
+++ plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/Atomic.java
@@ -13,8 +13,9 @@ import org.eclipse.ui.PlatformUI;
  */
 public class Atomic implements IApplication {
 	
-	// Define a static logger variable so that it references the
-	// Logger instance named "Atomic".
+	/** 
+	 * Defines a static logger variable so that it references the {@link Logger} instance named "Atomic".
+	 */
 	private static final Logger log = LogManager.getLogger(Atomic.class);
 	
 	/* (non-Javadoc)
