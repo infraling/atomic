8cadb95fa09215f4830c41c2ef041388f190130b
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-09-08 12:10:44 +0200

    Add exit command functionality

--- plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/Atomic.java
+++ plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/Atomic.java
@@ -12,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Contributors:
- *     Stephan Druskat - initial API and implementation
  *******************************************************************************/
 package de.uni_jena.iaa.linktype.atomic;
 
