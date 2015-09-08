c6967f3b8fad7b445f31b014c02569ff987ea218
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-09-08 12:40:18 +0200

    Add navigation view and navigation perspective

--- plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/Atomic.java
+++ plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/Atomic.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Contributors:
+ * 		initial API and implementation: Stephan Druskat
  *******************************************************************************/
 package de.uni_jena.iaa.linktype.atomic;
 
---
+++ plugins/de.uni_jena.iaa.linktype.atomic/src/main/java/de/uni_jena/iaa/linktype/atomic/perspectives/NavigationPerspective.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright 2015 Friedrich-Schiller-Universität Jena
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
+package de.uni_jena.iaa.linktype.atomic.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * Defines Atomic's Navigation Perspective, especially its layout, through code.
+ *
+ * <p>@author Stephan Druskat <stephan.druskat@uni-jena.de>
+ *
+ */
+public class NavigationPerspective implements IPerspectiveFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
