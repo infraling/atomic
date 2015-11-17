8ae97b9e20665d943f0030cccacf704a1009a835
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-11-17 11:50:19 +0100

    Implement AtomicPepperConfiguration#findPepperHome()

--- plugins/org.corpus-tools.atomic.pepper/src/main/java/org/corpus_tools/atomic/pepper/update/AtomicPepperConfiguration.java
+++ plugins/org.corpus-tools.atomic.pepper/src/main/java/org/corpus_tools/atomic/pepper/update/AtomicPepperConfiguration.java
@@ -19,21 +19,31 @@
 package org.corpus_tools.atomic.pepper.update;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.launch.Framework;
 
+import de.hu_berlin.german.korpling.saltnpepper.pepper.cli.PepperStarterConfiguration;
 import de.hu_berlin.german.korpling.saltnpepper.pepper.common.PepperConfiguration;
 
 /**
  * TODO Description
  * <p>
+ * 
  * @author Stephan Druskat <stephan.druskat@uni-jena.de>
  */
 @SuppressWarnings("serial")
 public class AtomicPepperConfiguration extends PepperConfiguration {
+
+	/**
+	 * Defines a static logger variable so that it references the {@link org.apache.logging.log4j.Logger} instance named "AtomicPepperConfiguration".
+	 */
+	private static final Logger log = LogManager.getLogger(AtomicPepperConfiguration.class);
 
 	private static final String PROP_PEPPER_HOME = "pepper.home";
 
@@ -42,21 +52,45 @@ public class AtomicPepperConfiguration extends PepperConfiguration {
 	 */
 	public void load() {
 		File pepperHome = findPepperHome();
+		try {
+			System.setProperty(PepperStarterConfiguration.PROP_PEPPER_HOME, pepperHome.getAbsolutePath());
+			super.load(pepperHome);
+		}
+		catch (Exception e) {
+			log.error("Pepper home has not been found", e);
+		}
 	}
 
 	/**
-	 * TODO: Description
+	 * Tries to "find" the Pepper home directory, which is in fact the
+	 * Atomic home directory for the running Atomic instance,
+	 * be computing the parent of the parent of the location
+	 * of the {@link org.corpus_tools.atomic.pepper} bundle.
+	 * <br>
+	 * The dir structure should be:
+	 * .../atomic/plugins/org.corpus_tools.atomic.pepper/
 	 *
-	 * @return
+	 * @return pepperHome The computed File if its name equals "atomic", else null
 	 */
 	private File findPepperHome() {
-		String atomicHome = null;
+		File pepperHome = null;
 		Bundle atomicPepperBundle = FrameworkUtil.getBundle(this.getClass());
-		String atomicPepperBundleName = atomicPepperBundle.getSymbolicName();
-		System.err.println(atomicPepperBundleName);
-//		atomicPepperBundle = 
-//		System.setProperty(PROP_PEPPER_HOME, atomicHome);
-		return null;
+		if (atomicPepperBundle.getSymbolicName().equals("org.corpus_tools.atomic.pepper")) {
+			File atomicPepperBundleLocationFile = new File(atomicPepperBundle.getLocation());
+			File pepperBundleParentAsPluginsDirFile = atomicPepperBundleLocationFile.getParentFile();
+			File pepperHomeAsAtomicHomeAsPluginsDirParent = pepperBundleParentAsPluginsDirFile.getParentFile();
+			if (pepperHomeAsAtomicHomeAsPluginsDirParent.getName().equals("atomic")) {
+				pepperHome = pepperHomeAsAtomicHomeAsPluginsDirParent;
+				log.trace("Found Pepper home (Atomic home really...) at {}.", pepperHome.getAbsolutePath());
+			}
+			else {
+				log.error("The directory File computed from the bundle location of o.c-t.a.pepper > parent > parent is not the Atomic home directory!");
+			}
+		}
+		else {
+			log.error("Could not find Pepper home directory, computed from o.c-t.a.pepper bundle location > parent (plugins dir) > parent", new FileNotFoundException());
+		}
+		return pepperHome;
 	}
 
 }
