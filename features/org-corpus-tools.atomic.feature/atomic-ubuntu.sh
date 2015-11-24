50c7243620c1ae69f4285fba6a7bdcc70e9b70b9
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-11-24 15:04:46 +0100

    Remove duplicate shell script

--- features/org-corpus-tools.atomic.feature/atomic-ubuntu.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-echo "Setting the environment variable \"UBUNTU_MENUPROXY\" to \"0\".
-This has the effect that the Unity global menu is not used (on, e.g., Ubuntu).
-Instead, the menu is relocated to the application menu.
-If this isn't set, keyboard shortcut texts aren't visible in the menus."
-UBUNTU_MENUPROXY=0 ./atomic
