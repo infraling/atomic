213877ed965f4b63c6133187c2501bf25bb9a576
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-11-25 13:33:22 +0100

    Modify atomic-unity.sh to not use echo for command call

--- features/org-corpus-tools.atomic.feature/atomic-unity.sh
+++ features/org-corpus-tools.atomic.feature/atomic-unity.sh
@@ -48,4 +48,5 @@ echo "Setting the environment variable \"UBUNTU_MENUPROXY\" to \"0\".
 This has the effect that the Unity global menu is not used (on, e.g., Ubuntu).
 Instead, the menu is relocated to the application menu.
 If this isn't set, keyboard shortcut texts aren't visible in the menus."
-echo UBUNTU_MENUPROXY=0 ./atomic "$@"
+UBUNTU_MENUPROXY=0 ./atomic "$@"
+exit 0
