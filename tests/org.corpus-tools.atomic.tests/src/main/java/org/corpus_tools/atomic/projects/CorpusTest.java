c72fd7fd099e19c205f4605d6cf1a714ed796d33
Stephan Druskat <stephan.druskat@uni-jena.de>
2016-01-26 15:09:13 +0100

    Set up unit tests for Document and Corpus classes

---
+++ tests/org.corpus-tools.atomic.tests/src/main/java/org/corpus_tools/atomic/projects/CorpusTest.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright 2016 Friedrich-Schiller-Universität Jena
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
+package org.corpus_tools.atomic.projects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO Description
+ *
+ * <p>@author Stephan Druskat <stephan.druskat@uni-jena.de>
+ *
+ */
+public class CorpusTest {
+
+	/**
+	 * TODO: Description
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Corpus#Corpus(java.lang.String)}.
+	 */
+	@Test
+	public void testCorpus() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Corpus#value()}.
+	 */
+	@Test
+	public void testValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Corpus#getChildren()}.
+	 */
+	@Test
+	public void testGetChildren() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Corpus#addChild(org.corpus_tools.atomic.projects.ProjectNode)}.
+	 */
+	@Test
+	public void testAddChild() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Corpus#removeChild(java.lang.String)}.
+	 */
+	@Test
+	public void testRemoveChild() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Corpus#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ tests/org.corpus-tools.atomic.tests/src/main/java/org/corpus_tools/atomic/projects/DocumentTest.java
@@ -0,0 +1,123 @@
+/*******************************************************************************
+ * Copyright 2016 Friedrich-Schiller-Universität Jena
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
+package org.corpus_tools.atomic.projects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO Description
+ *
+ * <p>@author Stephan Druskat <stephan.druskat@uni-jena.de>
+ *
+ */
+public class DocumentTest {
+
+	/**
+	 * TODO: Description
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#Document(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testDocument() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#value()}.
+	 */
+	@Test
+	public void testValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#getChildren()}.
+	 */
+	@Test
+	public void testGetChildren() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#addChild(org.corpus_tools.atomic.projects.ProjectNode)}.
+	 */
+	@Test
+	public void testAddChild() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#removeChild(java.lang.String)}.
+	 */
+	@Test
+	public void testRemoveChild() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#getSourceText()}.
+	 */
+	@Test
+	public void testGetSourceText() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#setValue(java.lang.String)}.
+	 */
+	@Test
+	public void testSetValueString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#getLeft()}.
+	 */
+	@Test
+	public void testGetLeft() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.Document#getRight()}.
+	 */
+	@Test
+	public void testGetRight() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
