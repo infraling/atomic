22a8ab64ed6e9722229161f28074d28e8f9bee0b
Stephan Druskat <stephan.druskat@uni-jena.de>
2016-01-27 16:26:36 +0100

    Add test class for SaltProjectCompiler

---
+++ tests/org.corpus-tools.atomic.tests/src/main/java/org/corpus_tools/atomic/projects/SaltProjectCompilerTest.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright 2016 Friedrich-Schiller-Universität Jena
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Stephan Druskat - initial API and implementation
+ *******************************************************************************/
+package org.corpus_tools.atomic.projects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link SaltProjectCompiler}.
+ *
+ * <p>@author Stephan Druskat <stephan.druskat@uni-jena.de>
+ *
+ */
+public class SaltProjectCompilerTest {
+	
+	private SaltProjectCompiler fixture = null;
+
+	/**
+	 * Set up the fixture.
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+//		setFixture(new SaltProjectCompiler(projectData));
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.SaltProjectCompiler#SaltProjectCompiler(org.corpus_tools.atomic.projects.ProjectData)}.
+	 */
+	@Test
+	public void testSaltProjectCompiler() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.SaltProjectCompiler#run()}.
+	 */
+	@Test
+	public void testRun() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.projects.SaltProjectCompiler#setProjectData(org.corpus_tools.atomic.projects.ProjectData)}.
+	 */
+	@Test
+	public void testSetProjectData() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * @return the fixture
+	 */
+	private SaltProjectCompiler getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * @param fixture the fixture to set
+	 */
+	private void setFixture(SaltProjectCompiler fixture) {
+		this.fixture = fixture;
+	}
+
+}
