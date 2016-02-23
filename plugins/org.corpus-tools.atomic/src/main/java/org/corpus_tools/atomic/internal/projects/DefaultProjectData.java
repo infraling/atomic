40d23f887a8a4503f1b84decbb352e0db872e580
Stephan Druskat <stephan.druskat@uni-jena.de>
2016-02-23 13:22:19 +0100

    Refactor DefaultProjectData to adhere to AbstractBean props

--- plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/internal/projects/DefaultProjectData.java
+++ plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/internal/projects/DefaultProjectData.java
@@ -19,29 +19,35 @@
 package org.corpus_tools.atomic.internal.projects;
 
 import java.util.HashMap;
-
+import org.corpus_tools.atomic.models.AbstractBean;
 import org.corpus_tools.atomic.projects.ProjectData;
 import org.corpus_tools.atomic.projects.ProjectNode;
+import org.eclipse.core.runtime.Assert;
 
 /**
  * Default implementation of {@link ProjectData}. This implementation
- * notably uses a {@link HashSet} to store the project's
+ * notably uses a {@link HashMap} to store the project's
  * root corpora.
  * <p>
  * @author Stephan Druskat <stephan.druskat@uni-jena.de>
  */
-public class DefaultProjectData implements ProjectData {
-
-	private String name;
-	private HashMap<String, ProjectNode> corpora = new HashMap<>();
+public class DefaultProjectData extends AbstractBean implements ProjectData {
+	
+	/**
+	 * Property <code>name</name>, readable and writable.
+	 */
+	private String name = null;
 
 	/**
-	 * Constructor taking the name of the project.
-	 * 
-	 * @param name
+	 * Property <code>corpora</name>, readable and writable.
 	 */
-	public DefaultProjectData(String name) {
-		this.name = name;
+	private HashMap<String, ProjectNode> corpora = null;
+	
+	/**
+	 * Default no-arg constructor (JavaBean compliance). 
+	 */
+	public DefaultProjectData() {
+		corpora = new HashMap<>();
 	}
 
 	/*
@@ -52,6 +58,15 @@ public class DefaultProjectData implements ProjectData {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(final String name) {
+		final String oldName = this.name;
+		this.name = name;
+		firePropertyChange("name", oldName, this.name);
+	}
+
 	/*
 	 * @copydoc @see org.corpus_tools.atomic.projects.ProjectData#getCorpora()
 	 */
@@ -60,20 +75,36 @@ public class DefaultProjectData implements ProjectData {
 		return corpora;
 	}
 
+	/**
+	 * @param corpora the corpora to set
+	 */
+	public void setCorpora(final HashMap<String, ProjectNode> corpora) {
+		final HashMap<String, ProjectNode> oldCorpora = this.corpora;
+		this.corpora = corpora;
+		firePropertyChange("corpora", oldCorpora, this.corpora);
+	}
+
 	/*
 	 * @copydoc @see org.corpus_tools.atomic.projects.ProjectData#addCorpus(org.corpus_tools.atomic.projects.ProjectNode)
 	 */
 	@Override
-	public void addCorpus(ProjectNode corpus) {
-		getCorpora().put(corpus.getName(), corpus);
+	public void addCorpus(final ProjectNode corpus) {
+		Assert.isNotNull(corpus);
+		final HashMap<String,ProjectNode> newCorpora = getCorpora();
+		newCorpora.put(corpus.getName(), corpus);
+		setCorpora(newCorpora);
 	}
 
 	/*
 	 * @copydoc @see org.corpus_tools.atomic.projects.ProjectData#removeCorpus(org.corpus_tools.atomic.projects.ProjectNode)
 	 */
 	@Override
-	public ProjectNode removeCorpus(String corpusName) {
-		return getCorpora().remove(corpusName);
+	public ProjectNode removeCorpus(final String corpusName) {
+		Assert.isNotNull(corpusName);
+		final HashMap<String, ProjectNode> newCorpora = getCorpora();
+		ProjectNode removedCorpus = newCorpora.remove(corpusName);
+		setCorpora(newCorpora);
+		return removedCorpus;
 	}
 
 }
