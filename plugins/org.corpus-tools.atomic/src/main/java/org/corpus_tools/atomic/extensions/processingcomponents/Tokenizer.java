86f731f2ae0af5818db7380e9bb3d80d3c506327
Stephan Druskat <stephan.druskat@uni-jena.de>
2016-03-17 14:54:56 +0100

    Implement Tokenizer

--- plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/extensions/processingcomponents/Tokenizer.java
+++ plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/extensions/processingcomponents/Tokenizer.java
@@ -22,18 +22,39 @@ import java.util.List;
 
 import org.corpus_tools.atomic.extensions.ProcessingComponent;
 import org.corpus_tools.atomic.extensions.ProcessingException;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
 
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SDocument;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.STextualDS;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SToken;
 
 /**
- * TODO Description
+ * A semi-abstract tokenizer for raw corpus document source texts. This class
+ * provides a default implementation for {@link #processDocument(SDocument)} which
+ * uses the {@link List<String>} returned by {@link #tokenize(String)} to
+ * create tokens for all {@link STextualDS}s in the {@link SDocument}s
+ * {@link SDocumentGraph}.
+ * <p>
+ * Clients extending this class can choose to 
+ * <ul>
+ * <li>override {@link #tokenize(String)}, which is the simplest option</li>
+ * <li>override {@link #processDocument(SDocument)} which means that they
+ * need to handle the {@link SDocument} itself</li>
+ * <li>Override both methods for maximum flexibility.</li>
+ * <p>
+ * This class is meant to be used with Salt 2.1.1.
+ * <p>
+ * @see <a href="https://github.com/korpling/salt/releases/tag/salt-2.1.1">Salt version 2.1.1</a>
+ * @see <a href="http://corpus-tools.org/salt">http://corpus-tools.org/salt</a>
  *
  * @author Stephan Druskat <mail@sdruskat.net>
  *
  */
 public abstract class Tokenizer implements ProcessingComponent<String, List<String>> {
+	
+	// FIXME: Needs unit tests!
 
 	/**
 	 * Takes a {@link String} (here the raw source text of a corpus document) and tokenizes it, 
@@ -56,35 +77,38 @@ public abstract class Tokenizer implements ProcessingComponent<String, List<Stri
 		if (documentGraph.getSTextualDSs().get(0) == null) {
 			throw new ProcessingException("The processed document's document graph does not have an STextualDS!", new NullPointerException());
 		}
-		// FIXME: For all stextualdss, do the following!
-		String documentSourceText = documentGraph.getSTextualDSs().get(0).getSText(); // We assume that there is only one STextualDS in the SDocumentGraph
-		List<String> tokens = tokenize(documentSourceText);
-		
-		// From SDocumentGraphImpl
-		if ((tokens != null) && (tokens.size() > 0)) {
-			char[] chrText = documentSourceText.toCharArray();
-			int tokenCntr = 0;
+		for (STextualDS sTextualDS : documentGraph.getSTextualDSs()) {
+			String documentSourceText = sTextualDS.getSText(); // We assume that there is only one STextualDS in the SDocumentGraph
+			List<String> tokens = tokenize(documentSourceText);
 
-			for (int i = 0; i < chrText.length; i++) {
-				if ((tokens.get(tokenCntr).length() < 1) || (tokens.get(tokenCntr).substring(0, 1).equals(String.valueOf(chrText[i])))) { // first letter matches
-					StringBuffer pattern = new StringBuffer();
-					for (int y = 0; y < tokens.get(tokenCntr).length(); y++) {// compute pattern in text
-						pattern.append(chrText[i + y]);
-					} // compute pattern in text
-					if (tokens.get(tokenCntr).hashCode() == pattern.toString().hashCode()) {// pattern found
-						int start = i;
-						int end = i + documentSourceText.length() + tokens.get(tokenCntr).length();
+			// Adapted from de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.impl.SDocumentGraphImpl
+			if ((tokens != null) && (tokens.size() > 0)) {
+				EList<SToken> tokenEList = null;
+				char[] documentSourceTextChars = documentSourceText.toCharArray();
+				int tokenCounter = 0;
+				for (int i = 0; i < documentSourceTextChars.length; i++) {
+					if ((tokens.get(tokenCounter).length() < 1) || (tokens.get(tokenCounter).substring(0, 1).equals(String.valueOf(documentSourceTextChars[i])))) { // first letter matches
+						StringBuffer pattern = new StringBuffer();
+						for (int y = 0; y < tokens.get(tokenCounter).length(); y++) {// compute pattern in text
+							pattern.append(documentSourceTextChars[i + y]);
+						}
+						if (tokens.get(tokenCounter).hashCode() == pattern.toString().hashCode()) {// pattern found
+							int start = i;
+							int end = i + documentSourceText.length() + tokens.get(tokenCounter).length();
 
-						SToken sTok = documentGraph.createSToken(sTextualDS, start, end);
-						if (retVal == null)
-							retVal = new BasicEList<SToken>();
-						retVal.add(sTok);
-						i = i + tokens.get(tokenCntr).length() - 1;
-						tokenCntr++;
-						if (tokenCntr >= tokens.size())
-							break;
-					} // pattern found
-				} // first letter matches
+							SToken sTok = documentGraph.createSToken(sTextualDS, start, end);
+							if (tokenEList == null) {
+								tokenEList = new BasicEList<SToken>();
+							}
+							tokenEList.add(sTok);
+							i = i + tokens.get(tokenCounter).length() - 1;
+							tokenCounter++;
+							if (tokenCounter >= tokens.size()) {
+								break;
+							}
+						}
+					}
+				}
 			}
 		}
 	}
