b3ed4be6721a4304bbfdd49b9254bd913e9cb76c
Stephan Druskat <stephan.druskat@uni-jena.de>
2016-03-25 22:29:16 +0100

    Start implementation of NewAtomicProjectWizardPageProcessingPipeline

--- plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/projects/wizard/NewAtomicProjectWizardPageProcessingPipeline.java
+++ plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/projects/wizard/NewAtomicProjectWizardPageProcessingPipeline.java
@@ -18,9 +18,17 @@
  *******************************************************************************/
 package org.corpus_tools.atomic.projects.wizard;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
@@ -51,9 +59,41 @@ public class NewAtomicProjectWizardPageProcessingPipeline extends WizardPage {
 	@Override
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(new Gr);
-		Label label = new Label(container, SWT.NONE);
-		label.setText("Nothing to see here!");
+		container.setLayout(new GridLayout(1, true));
+		
+		Composite top = new Composite(container, SWT.NONE);
+		top.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		top.setLayout(new FillLayout());
+		
+		IConfigurationElement[] tokenizers = Platform.getExtensionRegistry().getConfigurationElementsFor("org.corpus_tools.atomic.processingComponents.tokenizers");
+		
+		CTabFolder tabfolder = new CTabFolder(top, SWT.NONE);
+		tabfolder.setSimple(true);
+		tabfolder.setBorderVisible(true);
+//		tabfolder.set
+		CTabItem tokenizerTab = new CTabItem(tabfolder, SWT.NONE);
+		tokenizerTab.setText("Tokenizers");
+		Composite tokenizerControl = new Composite(tabfolder, SWT.NONE);
+		tokenizerControl.setLayout(new RowLayout());
+		for (IConfigurationElement tokenizer : tokenizers) {
+			Button button = new Button(tokenizerControl, SWT.NONE);
+			button.setText(tokenizer.getAttribute("name"));
+		}
+		tokenizerTab.setControl(tokenizerControl);
+		
+		CTabItem placeholder1 = new CTabItem(tabfolder, SWT.NONE);
+		placeholder1.setText("(Segmenters)");
+		CTabItem placeholder2 = new CTabItem(tabfolder, SWT.NONE);
+		placeholder2.setText("Placeholders");
+		CTabItem placeholder3 = new CTabItem(tabfolder, SWT.NONE);
+		placeholder3.setText("More placeholders");
+		
+		Composite bottom = new Composite(container, SWT.NONE);
+		bottom.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		bottom.setLayout(new FillLayout());
+		Label label2 = new Label(bottom, SWT.NONE);
+		label2.setText("Nothing to see here either!");
+		
 		setControl(container);
 	}
 
