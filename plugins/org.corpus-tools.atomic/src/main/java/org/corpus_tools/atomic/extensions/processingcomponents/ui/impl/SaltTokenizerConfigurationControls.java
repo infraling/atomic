83e182fa68dd78a04464824651bc9bc235af280f
Stephan Druskat <stephan.druskat@uni-jena.de>
2016-05-20 11:49:57 +0200

    Add file-reading for abbreviations

--- plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/extensions/processingcomponents/ui/impl/SaltTokenizerConfigurationControls.java
+++ plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/extensions/processingcomponents/ui/impl/SaltTokenizerConfigurationControls.java
@@ -18,7 +18,14 @@
  *******************************************************************************/
 package org.corpus_tools.atomic.extensions.processingcomponents.ui.impl;
 
+import java.io.BufferedReader; 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.corpus_tools.atomic.databinding.converters.HashSetToALinebrokenStringConverter;
 import org.corpus_tools.atomic.databinding.converters.LinebrokenStringToHashSetConverter;
 import org.corpus_tools.atomic.extensions.processingcomponents.impl.ProcessingComponentConfigurationProperties;
 import org.corpus_tools.atomic.extensions.processingcomponents.impl.SaltTokenizer;
@@ -35,9 +42,13 @@ import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -49,12 +60,17 @@ import com.neovisionaries.i18n.LanguageCode;
  * @author Stephan Druskat <mail@sdruskat.net>
  */
 public class SaltTokenizerConfigurationControls extends ProcessingComponentConfigurationControls {
-
+	
+	/** 
+	 * Defines a static logger variable so that it references the {@link org.apache.logging.log4j.Logger} instance named "SaltTokenizerConfigurationControls".
+	 */
+	private static final Logger log = LogManager.getLogger(SaltTokenizerConfigurationControls.class);
+	
 	private ComboViewer languageCodeCombo;
 	
-	private SaltTokenizerConfiguration configuration = null;
-
 	private Text abbreviationsText;
+	
+	private static final String ABBREVIATIONS_TEXT = "Enter comma-separated list of abbreviations or browse for abbreviations file ...\n" + "Abbreviations files must contain one abbreviation per line, e.g.:\n" + "Abk.\n" + "Bzgl.\n" + "d.h.";
 
 	/*
 	 * @copydoc @see org.corpus_tools.atomic.extensions.processingcomponents.ui.ProcessingComponentConfigurationControls#addControls()
@@ -79,21 +95,53 @@ public class SaltTokenizerConfigurationControls extends ProcessingComponentConfi
 		});
 		languageCodeCombo.setInput(LanguageCode.values());
 		languageCodeCombo.getCombo().select(0);
+		languageCodeCombo.getControl().setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
 
 		Label abbreviationsLabel = new Label(parent, SWT.NONE);
 		abbreviationsLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, false, false, 1, 1));
 		abbreviationsLabel.setText("Language-specific abbreviations");
 
 		abbreviationsText = new Text(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
-		abbreviationsText.setText("Enter comma-separated list of abbreviations or browse for abbreviations file ...\n" + "Abbreviations files must contain one abbreviation per line, e.g.:\n" + "Abk.\n" + "Bzgl.\n" + "d.h.");
 		GridData abbreviationsTextLayoutData = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
 		abbreviationsTextLayoutData.heightHint = 5 * abbreviationsText.getLineHeight();
+		abbreviationsTextLayoutData.widthHint = 600;
 		abbreviationsText.setLayoutData(abbreviationsTextLayoutData);
 		setProperty(abbreviationsText, ProcessingComponentConfigurationProperties.SaltTokenizerConfiguration_abbreviations);
+		abbreviationsText.setText(ABBREVIATIONS_TEXT);
 
 		Button abbreviationsFileBrowseButton = new Button(parent, SWT.PUSH);
 		abbreviationsFileBrowseButton.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
-		abbreviationsFileBrowseButton.setText("Browse ...");
+		abbreviationsFileBrowseButton.setText("&Browse ...");
+		abbreviationsFileBrowseButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog dialog = new FileDialog(Display.getCurrent() != null ? Display.getCurrent().getActiveShell() : Display.getDefault().getActiveShell(), SWT.OPEN | SWT.SINGLE);
+				String path = dialog.open();
+				if (path != null) {
+					File abbreviationsFile = new File(path);
+					StringBuffer stringBuffer = new StringBuffer();
+					try (BufferedReader br = new BufferedReader(new FileReader(abbreviationsFile))) {
+						for (String line; (line = br.readLine()) != null;) {
+							stringBuffer.append(line);
+							stringBuffer.append("\n");
+						}
+						br.close();
+						abbreviationsText.setText(stringBuffer.toString());
+					}
+					catch (IOException e1) {
+						log.error("Could not process file \"{}\".", abbreviationsFile, e1);
+					}
+				}
+			}
+		});
+
+		Label placeHolder = new Label(parent, SWT.WRAP);
+		placeHolder.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+		placeHolder.setVisible(false);
+		
+		Label abbreviationsDescription = new Label(parent, SWT.WRAP);
+		abbreviationsDescription.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
+		abbreviationsDescription.setText(ABBREVIATIONS_TEXT);
 
 		initDataBindings();
 	}
@@ -102,28 +150,18 @@ public class SaltTokenizerConfigurationControls extends ProcessingComponentConfi
 	 * Initializes the data bindings for the widgets working on actual data.
 	 */
 	private void initDataBindings() {
+		getConfiguration().setLanguageCode(LanguageCode.aa);
+		
 		DataBindingContext bindingContext = new DataBindingContext();
 		IViewerObservableValue languageCodewidgetObservable = ViewersObservables.observeSingleSelection(languageCodeCombo);
 		bindingContext.bindValue(languageCodewidgetObservable, BeanProperties.value(getConfiguration().getClass(), ProcessingComponentConfigurationProperties.SaltTokenizerConfiguration_languageCode).observe(getConfiguration()));
 
 		UpdateValueStrategy targetStrategy = new UpdateValueStrategy();
+		UpdateValueStrategy modelStrategy = new UpdateValueStrategy();
 		targetStrategy.setConverter(new LinebrokenStringToHashSetConverter(String.class, HashSet.class));
+		modelStrategy.setConverter(new HashSetToALinebrokenStringConverter(HashSet.class, String.class));
 		IObservableValue abbreviationsWidgetObservable = WidgetProperties.text(SWT.Modify).observe(abbreviationsText);
-		bindingContext.bindValue(abbreviationsWidgetObservable, BeanProperties.value(getConfiguration().getClass(), ProcessingComponentConfigurationProperties.SaltTokenizerConfiguration_abbreviations).observe(getConfiguration()), targetStrategy, null);
-	}
-
-	/**
-	 * @return the configuration
-	 */
-	public SaltTokenizerConfiguration getConfiguration() {
-		return configuration;
-	}
-
-	/**
-	 * @param configuration the configuration to set
-	 */
-	public void setConfiguration(SaltTokenizerConfiguration configuration) {
-		this.configuration = configuration;
+		bindingContext.bindValue(abbreviationsWidgetObservable, BeanProperties.value(getConfiguration().getClass(), ProcessingComponentConfigurationProperties.SaltTokenizerConfiguration_abbreviations).observe(getConfiguration()), targetStrategy, modelStrategy);
 	}
 
 }
