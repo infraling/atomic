8cd143ea67e0050ccf839a912fd14d616a67a06c
Stephan Druskat <mail@sdruskat.net>
2017-01-03 15:00:13 +0100

    Implement basic load graph functionality for token editor

---
+++ plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/TableBasedTokenEditor.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.corpus_tools.atomic.tokeneditor;
+
+import org.corpus_tools.atomic.editors.DocumentGraphEditor;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * TODO Description
+ *
+ * @author Stephan Druskat <mail@sdruskat.net>
+ *
+ */
+public class TableBasedTokenEditor extends DocumentGraphEditor {
+
+	/**
+	 * 
+	 */
+	public TableBasedTokenEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.corpus_tools.atomic.editors.DocumentGraphEditor#createEditorPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createEditorPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
