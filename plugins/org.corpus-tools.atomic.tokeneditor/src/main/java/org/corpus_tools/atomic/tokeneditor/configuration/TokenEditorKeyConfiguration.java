813a39c1c5fbea2e471a31ac6f6d241a2bfb058c
Stephan Druskat <mail@sdruskat.net>
2017-01-19 14:42:39 +0100

    Decouple TokenEditorKeyConfiguration.java

--- plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/configuration/TokenEditorKeyConfiguration.java
+++ plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/configuration/TokenEditorKeyConfiguration.java
@@ -3,13 +3,16 @@
  */
 package org.corpus_tools.atomic.tokeneditor.configuration;
 
-import org.corpus_tools.atomic.tokeneditor.TableBasedTokenEditor;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.config.AbstractUiBindingConfiguration;
+import org.eclipse.nebula.widgets.nattable.grid.layer.GridLayer;
+import org.eclipse.nebula.widgets.nattable.layer.ILayer;
+import org.eclipse.nebula.widgets.nattable.layer.IUniqueIndexLayer;
 import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
 import org.eclipse.nebula.widgets.nattable.ui.action.IKeyAction;
 import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
 import org.eclipse.nebula.widgets.nattable.ui.matcher.KeyEventMatcher;
+import org.eclipse.nebula.widgets.nattable.viewport.ViewportLayer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.KeyEvent;
@@ -39,12 +42,13 @@ public class TokenEditorKeyConfiguration extends AbstractUiBindingConfiguration 
 	public void configureUiBindings(UiBindingRegistry uiBindingRegistry) {
 		
 		/*
-		 * CTRL + TAB sets the focus on the text control.
+		 * CTRL + T sets the focus on the text control.
 		 */
 		uiBindingRegistry.registerKeyBinding(new KeyEventMatcher(SWT.CTRL, 't'), new IKeyAction() {
 			
 			@Override
 			public void run(NatTable natTable, KeyEvent event) {
+				syncTextPosition(natTable);
 				text.setFocus();
 			}
 		});			
@@ -61,7 +65,27 @@ public class TokenEditorKeyConfiguration extends AbstractUiBindingConfiguration 
 			
 			@Override
 			public void run(NatTable natTable, KeyEvent event) {
-				SelectionLayer selectionLayer = ((SelectionLayer) natTable.getData(TableBasedTokenEditor.DATA_SELECTIONLAYER));
+				syncTextPosition(natTable);
+			}
+		});
+
+	}
+
+	/**
+	 * TODO: Description
+	 *
+	 * @param natTable
+	 */
+	private void syncTextPosition(NatTable natTable) {
+		SelectionLayer selectionLayer = null;
+		ILayer underlyingLayer = natTable.getLayer();
+		if (underlyingLayer instanceof GridLayer) {
+			ILayer bodyLayer = ((GridLayer) underlyingLayer).getBodyLayer();
+			if (bodyLayer instanceof ViewportLayer) {
+				IUniqueIndexLayer scrollLayer = ((ViewportLayer) bodyLayer).getScrollableLayer();
+				if (scrollLayer instanceof SelectionLayer) {
+					selectionLayer = (SelectionLayer) scrollLayer;
+				}
 				int selectedCol = selectionLayer.getSelectedColumnPositions()[0];
 				Object pos = selectionLayer.getDataValueByPosition(selectedCol, 1); // FIXME: Get int via constant in editor so that it cannot break if row number changes
 				if (pos instanceof String) {
@@ -72,8 +96,7 @@ public class TokenEditorKeyConfiguration extends AbstractUiBindingConfiguration 
 					text.setSelection(start, end);
 				}
 			}
-		});
-
+		}
 	}
 
 }
