5183343a59bad97eb84a424feffcced7e89e9310
Stephan Druskat <mail@sdruskat.net>
2017-01-19 14:43:43 +0100

    Implement NewTokenHandler extending DocumentGraphAwareHandler

--- plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/handlers/NewTokenHandler.java
+++ plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/handlers/NewTokenHandler.java
@@ -3,9 +3,16 @@
  */
 package org.corpus_tools.atomic.tokeneditor.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
+import org.corpus_tools.atomic.api.commands.DocumentGraphAwareHandler;
+import org.corpus_tools.salt.common.STextualRelation;
+import org.corpus_tools.salt.common.SToken;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * TODO Description
@@ -13,15 +20,49 @@ import org.eclipse.core.commands.ExecutionException;
  * @author Stephan Druskat <mail@sdruskat.net>
  *
  */
-public class NewTokenHandler extends AbstractHandler {
+public class NewTokenHandler extends DocumentGraphAwareHandler {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.
+	 * ExecutionEvent)
 	 */
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.err.println("CREATE NEW TOKEN");
+		SToken selectedToken = null;
+		Widget widget;
+		if (getGraph() != null) {
+			if (!((widget = ((Event) event.getTrigger()).widget) instanceof StyledText)) {
+				selectedToken = getGraph().getSortedTokenByText().get(((int[])((StructuredSelection) HandlerUtil.getCurrentSelectionChecked(event)).getFirstElement())[0]);
+			}
+			/*
+			 * It's expensive to get the respective token for a StyledText's
+			 * caret position with every navigation stepin the text, so instead,
+			 * execute the necessary logic here.  
+			 */
+			else {
+				if ((widget = ((Event) event.getTrigger()).widget) instanceof StyledText) {
+					StyledText text = (StyledText) widget;
+					int caretPosition = text.getCaretOffset();
+					relations: for (STextualRelation rel : getGraph().getTextualRelations()) {
+						if (rel.getEnd() == caretPosition || rel.getEnd() == caretPosition) {
+							selectedToken = rel.getSource();
+							break relations;
+						}
+						else if (rel.getEnd() > caretPosition) {
+							if (rel.getStart() < caretPosition) {
+								selectedToken = rel.getSource();
+								break relations;
+							}
+						}
+					}
+				}
+			}
+		}
+		// FIXME
+		System.err.println(getGraph().getText(selectedToken));
 		return null;
 	}
-
 }
