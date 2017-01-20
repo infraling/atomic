083a15d85d3eec8304879820bf020461d44316f5
Stephan Druskat <mail@sdruskat.net>
2017-01-20 13:26:37 +0100

    Add handling of menu event to NewTokenHandler

--- plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/handlers/NewTokenHandler.java
+++ plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/handlers/NewTokenHandler.java
@@ -9,6 +9,7 @@ import org.corpus_tools.salt.common.SToken;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Widget;
@@ -32,37 +33,43 @@ public class NewTokenHandler extends DocumentGraphAwareHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		SToken selectedToken = null;
-		Widget widget;
+		Event trigger = (Event) event.getTrigger();
+		Widget widget = trigger.widget;
+		Object selectionElement = ((StructuredSelection) HandlerUtil.getCurrentSelectionChecked(event)).getFirstElement();
 		if (getGraph() != null) {
-			if (!((widget = ((Event) event.getTrigger()).widget) instanceof StyledText)) {
-				selectedToken = getGraph().getSortedTokenByText().get(((int[])((StructuredSelection) HandlerUtil.getCurrentSelectionChecked(event)).getFirstElement())[0]);
+			if (widget instanceof NatTable) {
+				if (selectionElement instanceof int[]) {
+					int[] selection = (int[]) selectionElement;
+					selectedToken = getGraph().getSortedTokenByText().get(selection[0]);
+				}
+				else if (trigger.data instanceof SToken){ // Command executed from menu 
+					selectedToken = (SToken) trigger.data;
+				}
 			}
 			/*
 			 * It's expensive to get the respective token for a StyledText's
 			 * caret position with every navigation stepin the text, so instead,
 			 * execute the necessary logic here.  
 			 */
-			else {
-				if ((widget = ((Event) event.getTrigger()).widget) instanceof StyledText) {
-					StyledText text = (StyledText) widget;
-					int caretPosition = text.getCaretOffset();
-					relations: for (STextualRelation rel : getGraph().getTextualRelations()) {
-						if (rel.getEnd() == caretPosition || rel.getEnd() == caretPosition) {
+			else if (widget instanceof StyledText) {
+				StyledText text = (StyledText) widget;
+				int caretPosition = text.getCaretOffset();
+				relations: for (STextualRelation rel : getGraph().getTextualRelations()) {
+					if (rel.getEnd() == caretPosition || rel.getEnd() == caretPosition) {
+						selectedToken = rel.getSource();
+						break relations;
+					}
+					else if (rel.getEnd() > caretPosition) {
+						if (rel.getStart() <= caretPosition) {
 							selectedToken = rel.getSource();
 							break relations;
-						}
-						else if (rel.getEnd() > caretPosition) {
-							if (rel.getStart() < caretPosition) {
-								selectedToken = rel.getSource();
-								break relations;
-							}
 						}
 					}
 				}
 			}
 		}
 		// FIXME
-		System.err.println(getGraph().getText(selectedToken));
+		System.err.println("HANDLE " + getGraph().getText(selectedToken));
 		return null;
 	}
 }
