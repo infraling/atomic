4ccbf1012f67ebb8e6045c3c02d17340a0851329
Stephan Druskat <mail@sdruskat.net>
2017-01-20 13:25:27 +0100

    Handle popup menu selection on NatTable cell correctly
    - See: [this StackOverflow question](http://stackoverflow.com/questions/34748101/showing-nattable-context-menu)

--- plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/configuration/EditorPopupMenuConfiguration.java
+++ plugins/org.corpus-tools.atomic.tokeneditor/src/main/java/org/corpus_tools/atomic/tokeneditor/configuration/EditorPopupMenuConfiguration.java
@@ -8,20 +8,29 @@ import org.corpus_tools.salt.common.SToken;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.config.AbstractUiBindingConfiguration;
 import org.eclipse.nebula.widgets.nattable.grid.GridRegion;
+import org.eclipse.nebula.widgets.nattable.grid.layer.GridLayer;
 import org.eclipse.nebula.widgets.nattable.layer.IUniqueIndexLayer;
 import org.eclipse.nebula.widgets.nattable.layer.LayerUtil;
+import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
+import org.eclipse.nebula.widgets.nattable.selection.command.SelectCellCommand;
+import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
 import org.eclipse.nebula.widgets.nattable.ui.NatEventData;
+import org.eclipse.nebula.widgets.nattable.ui.action.IMouseAction;
 import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
 import org.eclipse.nebula.widgets.nattable.ui.matcher.MouseEventMatcher;
 import org.eclipse.nebula.widgets.nattable.ui.menu.IMenuItemProvider;
 import org.eclipse.nebula.widgets.nattable.ui.menu.MenuItemProviders;
-import org.eclipse.nebula.widgets.nattable.ui.menu.PopupMenuAction;
 import org.eclipse.nebula.widgets.nattable.ui.menu.PopupMenuBuilder;
+import org.eclipse.nebula.widgets.nattable.viewport.ViewportLayer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.IHandlerService;
 
 /**
  * TODO Description
@@ -41,7 +50,7 @@ public class EditorPopupMenuConfiguration extends AbstractUiBindingConfiguration
 
 	@Override
 	public void configureUiBindings(UiBindingRegistry uiBindingRegistry) {
-		uiBindingRegistry.registerMouseDownBinding(new MouseEventMatcher(SWT.NONE, GridRegion.BODY, MouseEventMatcher.RIGHT_BUTTON), new PopupMenuAction(this.menu));
+		uiBindingRegistry.registerMouseDownBinding(new MouseEventMatcher(SWT.NONE, GridRegion.BODY, MouseEventMatcher.RIGHT_BUTTON), new CellPopupMenuAction());
 	}
 
 	/**
@@ -68,17 +77,59 @@ public class EditorPopupMenuConfiguration extends AbstractUiBindingConfiguration
 			newTokenLabelsMenuItem.addSelectionListener(new SelectionAdapter() {
 				@Override
 				public void widgetSelected(SelectionEvent e) {
-					NatEventData natEventData = MenuItemProviders.getNatEventData(e);
-					NatTable natTable = natEventData.getNatTable();
-					int columnPosition = natEventData.getColumnPosition();
-					int rowPosition = natEventData.getRowPosition();
-
-					System.err.println("Selected token: " + natTable.getDataValueByPosition(columnPosition, rowPosition));
-					int absoluteTokenIndex = LayerUtil.convertColumnPosition(natTable, columnPosition, (IUniqueIndexLayer) natTable.getData("dataLayer"));
+					NatEventData data = (NatEventData) menu.getData(MenuItemProviders.NAT_EVENT_DATA_KEY);
+					NatTable natTable = data.getNatTable();
+					int columnPosition = data.getColumnPosition();
+					IUniqueIndexLayer bodyLayer = ((ViewportLayer) ((GridLayer) natTable.getLayer()).getBodyLayer()).getScrollableLayer();
+					int absoluteTokenIndex = LayerUtil.convertColumnPosition(natTable, columnPosition, bodyLayer);
 					SToken selectedToken = graph.getSortedTokenByText().get(absoluteTokenIndex);
-					// FIXME Now call NewTokenHandler
+					Event event = new Event();
+					event.data = selectedToken;
+					event.widget = natTable;
+					IHandlerService handlerService = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService(IHandlerService.class);
+					try {
+						handlerService.executeCommand("org.corpus_tools.atomic.tokeneditor.createToken", event);
+					}
+					catch (Exception ex) {
+						throw new RuntimeException("org.corpus_tools.atomic.tokeneditor.createToken not found");
+						// FIXME: Log
+					}
+
 				}
 			});
+		}
+
+	}
+
+	/**
+	 * TODO Description
+	 *
+	 * @author Stephan Druskat <mail@sdruskat.net>
+	 *
+	 */
+	public class CellPopupMenuAction implements IMouseAction {
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.eclipse.nebula.widgets.nattable.ui.action.IMouseAction#run(org.
+		 * eclipse.nebula.widgets.nattable.NatTable,
+		 * org.eclipse.swt.events.MouseEvent)
+		 */
+		@Override
+		public void run(NatTable natTable, MouseEvent event) {
+			int columnPosition = natTable.getColumnPositionByX(event.x);
+			int rowPosition = natTable.getRowPositionByY(event.y);
+
+			ILayerCell cell = natTable.getCellByPosition(columnPosition, rowPosition);
+
+			if (!cell.getDisplayMode().equals(DisplayMode.SELECT)) {
+				natTable.doCommand(new SelectCellCommand(natTable, columnPosition, rowPosition, false, false));
+			}
+
+			menu.setData(MenuItemProviders.NAT_EVENT_DATA_KEY, event.data);
+			menu.setVisible(true);
 		}
 
 	}
