0e739ee70a66a80ce49dcfcbe198a986022bd0df
Thomas Krause <thomaskrause@posteo.de>
2017-02-17 16:47:48 +0100

    Use SaltVisualizer to display annotation graph.
    # Conflicts:
    #	pom.xml

--- plugins/org.corpus-tools.atomic.visjs/src/main/java/org/corpus_tools/atomic/visjs/editors/SaltVisualizer.java
+++ plugins/org.corpus-tools.atomic.visjs/src/main/java/org/corpus_tools/atomic/visjs/editors/SaltVisualizer.java
@@ -1,6 +1,13 @@
 package org.corpus_tools.atomic.visjs.editors;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import javax.xml.stream.XMLStreamException;
+
 import org.corpus_tools.atomic.api.editors.DocumentGraphEditor;
+import org.corpus_tools.salt.util.VisJsVisualizer;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
 
@@ -24,6 +31,19 @@ public class SaltVisualizer extends  DocumentGraphEditor {
 	public void createEditorPartControl(Composite parent) {
 		Browser browser = new Browser(parent, PROP_TITLE);
 		browser.setUrl("http://corpus-tools.org/atomic/");
+
+		try {
+			Path tmpDir = Files.createTempDirectory("atomic-visjs-visualizer");
+			
+			new VisJsVisualizer(getGraph().getDocument()).visualize(org.eclipse.emf.common.util.URI.createFileURI(tmpDir.toString()));
+			
+			browser.setUrl(tmpDir.resolve("saltVisJs.html").toString());
+
+		} catch (IOException | XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
