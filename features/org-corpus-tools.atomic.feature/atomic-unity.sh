50bfb9cb8ed81e9dd9f3c0d478b7858723574543
Stephan Druskat <mail@sdruskat.net>
2017-02-21 12:13:51 +0100

    Change atomic-unity.sh (to trigger Travis)

--- features/org-corpus-tools.atomic.feature/atomic-unity.sh
+++ features/org-corpus-tools.atomic.feature/atomic-unity.sh
@@ -47,6 +47,6 @@
 echo "Setting the environment variable \"UBUNTU_MENUPROXY\" to \"0\".
 This has the effect that the Unity global menu is not used (on, e.g., Ubuntu).
 Instead, the menu is relocated to the application menu.
-If this isn't set, keyboard shortcut texts aren't visible in the menus."
+If this isn't set, keybinding hints aren't visible in the menus."
 UBUNTU_MENUPROXY=0 ./atomic "$@"
 exit 0
