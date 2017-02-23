9f73660341ba586069b300e8c82437f65cd2abe6
Thomas Krause <thomaskrause@posteo.de>
2017-02-23 17:41:43 +0100

    Use InjectionFXMLLoader

--- plugins/org.corpus-tools.atomic.search/src/main/java/org/corpus_tools/search/parts/ANNISSearch.java
+++ plugins/org.corpus-tools.atomic.search/src/main/java/org/corpus_tools/search/parts/ANNISSearch.java
@@ -3,16 +3,10 @@ package org.corpus_tools.search.parts;
 import java.io.IOException;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Named;
 
-import org.eclipse.e4.core.di.annotations.Optional;
-import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.fx.osgi.util.OSGiFXMLLoader;
-import org.eclipse.fx.osgi.util.OSGiFXMLLoader.FXMLData;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.fx.ui.di.FXMLBuilder.Data;
+import org.eclipse.fx.ui.di.InjectingFXMLLoader;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
@@ -27,98 +21,31 @@ public class ANNISSearch {
 
 	private FXCanvas fxCanvas;
 	
-	private void createFXMLScene() {
-		FXMLData<Parent, ANNISSearchController> fxmlData;
+	
+	private void createFXMLScene(IEclipseContext context) {
+		InjectingFXMLLoader<Parent> loader = InjectingFXMLLoader.create(context, getClass(), "ANNISSearch.fxml");
+		Data<Parent, ANNISSearchController> fxmlData;
 		try {
-			fxmlData = OSGiFXMLLoader.loadWithController(getClass(), "ANNISSearch.fxml", null, null);
-			this.controller = fxmlData.controller;
-			final Scene scene = new Scene(fxmlData.node);
+			fxmlData = loader.loadWithController();
+			this.controller = fxmlData.getController();
+			final Scene scene = new Scene(fxmlData.getNode());
 			fxCanvas.setScene(scene);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		}
+		
+
 	}
 	
 	@PostConstruct
-	public void createPartControl(Composite parent) {
+	public void createPartControl(Composite parent, IEclipseContext context) {
 		fxCanvas = new FXCanvas(parent, SWT.NONE);
 		
 		Platform.setImplicitExit(false);
-		Platform.runLater(() -> createFXMLScene());
+		Platform.runLater(() -> createFXMLScene(context));
 	}
 	
 
-	@Focus
-	public void setFocus() {
-		if(controller != null) {
-			controller.focus();
-		}
 
-	}
-
-	/**
-	 * This method is kept for E3 compatiblity. You can remove it if you do not
-	 * mix E3 and E4 code. <br/>
-	 * With E4 code you will set directly the selection in ESelectionService and
-	 * you do not receive a ISelection
-	 * 
-	 * @param s
-	 *            the selection received from JFace (E3 mode)
-	 */
-	@Inject
-	@Optional
-	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) ISelection s) {
-		if (s == null || s.isEmpty())
-			return;
-
-		if (s instanceof IStructuredSelection) {
-			IStructuredSelection iss = (IStructuredSelection) s;
-			if (iss.size() == 1)
-				setSelection(iss.getFirstElement());
-			else
-				setSelection(iss.toArray());
-		}
-	}
-
-	/**
-	 * This method manages the selection of your current object. In this example
-	 * we listen to a single Object (even the ISelection already captured in E3
-	 * mode). <br/>
-	 * You should change the parameter type of your received Object to manage
-	 * your specific selection
-	 * 
-	 * @param o
-	 *            : the current object received
-	 */
-	@Inject
-	@Optional
-	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object o) {
-
-		// Remove the 2 following lines in pure E4 mode, keep them in mixed mode
-		if (o instanceof ISelection) // Already captured
-			return;
-
-		// Test if label exists (inject methods are called before PostConstruct)
-		if (controller != null)
-			controller.showInfo("Current single selection class is : " + o.getClass());
-	}
-
-	/**
-	 * This method manages the multiple selection of your current objects. <br/>
-	 * You should change the parameter type of your array of Objects to manage
-	 * your specific selection
-	 * 
-	 * @param o
-	 *            : the current array of objects received in case of multiple
-	 *            selection
-	 */
-	@Inject
-	@Optional
-	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object[] selectedObjects) {
-
-		// Test if label exists (inject methods are called before PostConstruct)
-		if (controller != null)
-			controller.showInfo("This is a multiple selection of " + selectedObjects.length + " objects");
-	}
 }
--- plugins/org.corpus-tools.atomic.search/src/main/java/org/corpus_tools/search/parts/ANNISSearchController.java
+++ plugins/org.corpus-tools.atomic.search/src/main/java/org/corpus_tools/search/parts/ANNISSearchController.java
@@ -1,22 +1,37 @@
 package org.corpus_tools.search.parts;
 
+import javax.inject.Inject;
+
 import org.eclipse.e4.core.di.annotations.Creatable;
+import org.eclipse.e4.ui.di.UISynchronize;
+import org.osgi.service.log.LogService;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 @Creatable
 public class ANNISSearchController {
 
 	@FXML
-	private Label myLabelInView;
+	private Button reIndexButton;
 	
-	public void focus() {
-		myLabelInView.requestFocus();
+	@Inject
+	private UISynchronize uiSync;
+	
+	@Inject 
+	private LogService log;
+	
+	public void reindex() {
+		log.log(LogService.LOG_INFO, "Attempting re-index");
+		uiSync.asyncExec(() -> {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			};
+		} );
 	}
 	
-	public void showInfo(String msg) {
-		myLabelInView.setText(msg);
-	}
 	
 }
