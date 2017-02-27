10918ca00bbb7a075529116b5e502316c5c3be72
Thomas Krause <thomaskrause@posteo.de>
2017-02-27 14:48:56 +0100

    Add atomical console to menu.

--- plugins/de.uni_jena.iaa.linktype.atomic.atomical/src/main/java/de/uni_jena/iaa/linktype/atomic/atomical/console/OpenAtomicalConsoleHandler.java
+++ plugins/de.uni_jena.iaa.linktype.atomic.atomical/src/main/java/de/uni_jena/iaa/linktype/atomic/atomical/console/OpenAtomicalConsoleHandler.java
@@ -3,21 +3,17 @@
  */
 package de.uni_jena.iaa.linktype.atomic.atomical.console;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.e4.core.di.annotations.Execute;
 
 /**
  * @author Stephan Druskat
  *
  */
-public final class OpenAtomicalConsoleHandler extends AbstractHandler {
+public final class OpenAtomicalConsoleHandler {
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
-	 */
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+	@Execute
+	public Object execute() throws ExecutionException {
 		AtomicalConsoleFactory acf = new AtomicalConsoleFactory();
 		acf.openConsole();
 		return null;
