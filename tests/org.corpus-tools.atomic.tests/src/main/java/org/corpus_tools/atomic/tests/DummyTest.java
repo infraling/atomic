a7b0bea690f080f8d3b0ac7d485923a1c658577c
Thomas Krause <thomaskrause@posteo.de>
2017-02-27 11:49:48 +0100

    Add a dummy test to make Maven happy.

---
+++ tests/org.corpus-tools.atomic.tests/src/main/java/org/corpus_tools/atomic/tests/DummyTest.java
@@ -0,0 +1,14 @@
+package org.corpus_tools.atomic.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void dummyTest() {
+		// Don't test anything, just make maven happy.
+	}
+
+}
