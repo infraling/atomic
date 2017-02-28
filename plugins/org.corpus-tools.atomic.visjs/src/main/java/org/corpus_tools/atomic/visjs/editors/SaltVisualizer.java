b214286209d8587f4571f0cbbb033711172d6ff4
Thomas Krause <thomaskrause@posteo.de>
2017-02-28 18:00:41 +0100

    Allow to filter out spans.

--- plugins/org.corpus-tools.atomic.visjs/src/main/java/org/corpus_tools/atomic/visjs/editors/SaltVisualizer.java
+++ plugins/org.corpus-tools.atomic.visjs/src/main/java/org/corpus_tools/atomic/visjs/editors/SaltVisualizer.java
@@ -3,31 +3,41 @@ package org.corpus_tools.atomic.visjs.editors;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
 
-import javax.swing.plaf.FileChooserUI;
 import javax.xml.stream.XMLStreamException;
 
 import org.apache.commons.io.FileUtils;
 import org.corpus_tools.atomic.api.editors.DocumentGraphEditor;
+import org.corpus_tools.salt.common.SSpan;
+import org.corpus_tools.salt.core.SNode;
+import org.corpus_tools.salt.core.SRelation;
+import org.corpus_tools.salt.util.ExportFilter;
 import org.corpus_tools.salt.util.VisJsVisualizer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
-public class SaltVisualizer extends  DocumentGraphEditor {
+import swing2swt.layout.BorderLayout;
+
+public class SaltVisualizer extends DocumentGraphEditor {
 
 	private Path tmpDir;
+	private Browser browser;
+	private Button btnIncludeSpans;
 
 	public SaltVisualizer() {
 		super();
 	}
-	
-	
-	
+
 	@Override
 	public void dispose() {
-		
-		if(tmpDir != null) {
+
+		if (tmpDir != null) {
 			try {
 				FileUtils.deleteDirectory(tmpDir.toFile());
 			} catch (IOException e) {
@@ -35,17 +45,48 @@ public class SaltVisualizer extends  DocumentGraphEditor {
 				e.printStackTrace();
 			}
 		}
-		
+
 		super.dispose();
 	}
 
-
-
 	@Override
 	public void createEditorPartControl(Composite parent) {
-		Browser browser = new Browser(parent, PROP_TITLE);
+
+		parent.setLayout(new BorderLayout());
 		
-		if(tmpDir != null) {
+		browser = new Browser(parent, SWT.NONE);
+		
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayoutData(BorderLayout.EAST);
+		composite.setLayout(new GridLayout(1, false));
+
+		Label lblFilterByAnnotation = new Label(composite, SWT.NONE);
+		lblFilterByAnnotation.setText("Filter by annotation type");
+
+		btnIncludeSpans = new Button(composite, SWT.CHECK);
+		btnIncludeSpans.setSelection(true);
+		btnIncludeSpans.setText("Include Spans");
+		btnIncludeSpans.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				updateView();
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+		updateView();
+
+	}
+
+	private void updateView() {
+		if (tmpDir != null) {
 			try {
 				FileUtils.deleteDirectory(tmpDir.toFile());
 			} catch (IOException e) {
@@ -53,21 +94,40 @@ public class SaltVisualizer extends  DocumentGraphEditor {
 				e.printStackTrace();
 			}
 		}
-		
+
 		try {
-			
-			
 			tmpDir = Files.createTempDirectory("atomic-visjs-visualizer-");
+
+			VisJsVisualizer visjs = new VisJsVisualizer(getGraph().getDocument(), new Filter(), null);
 			
-			new VisJsVisualizer(getGraph().getDocument()).visualize(org.eclipse.emf.common.util.URI.createFileURI(tmpDir.toString()));
+			visjs.visualize(org.eclipse.emf.common.util.URI.createFileURI(tmpDir.toString()));
 			
 			browser.setUrl(tmpDir.resolve("saltVisJs.html").toString());
-
+			
 		} catch (IOException | XMLStreamException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+	
+	private class Filter implements ExportFilter {
+
+		@Override
+		public boolean includeNode(SNode node) {
+			
+			if(node instanceof SSpan) {
+				return btnIncludeSpans.getSelection();
+			}
+			
+			// default to true
+			return true;
+		}
+
+		@Override
+		public boolean includeRelation(SRelation arg0) {
+			
+			return true;
+		}
 		
 	}
 
