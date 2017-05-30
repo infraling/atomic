b27e343772c7a2e5a4d9c042cf5fba795ddee4c7
Stephan Druskat <mail@sdruskat.net>
2017-05-30 15:43:25 +0200

    Configure grid to open adjacent cell editors after commit

--- plugins/org.corpus-tools.atomic.grideditor/src/main/java/org/corpus_tools/atomic/grideditor/configuration/GridEditConfiguration.java
+++ plugins/org.corpus-tools.atomic.grideditor/src/main/java/org/corpus_tools/atomic/grideditor/configuration/GridEditConfiguration.java
@@ -12,6 +12,7 @@ import org.eclipse.nebula.widgets.nattable.data.convert.DisplayConverter;
 import org.eclipse.nebula.widgets.nattable.data.convert.IDisplayConverter;
 import org.eclipse.nebula.widgets.nattable.edit.EditConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.edit.editor.TextCellEditor;
+import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer.MoveDirectionEnum;
 import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
 
 /**
@@ -27,9 +28,16 @@ public class GridEditConfiguration extends AbstractRegistryConfiguration {
 	 */
 	@Override
 	public void configureRegistry(IConfigRegistry configRegistry) {
+		// Make cells editable
 		configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITABLE_RULE, IEditableRule.ALWAYS_EDITABLE);
+		// Configure what to show in the grid
 		configRegistry.registerConfigAttribute(CellConfigAttributes.DISPLAY_CONVERTER, getAnnotationValueConverter(), DisplayMode.NORMAL);
-		configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITOR, new TextCellEditor(), DisplayMode.EDIT, null);
+		// Configure cell editing
+		configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITOR, new AnnotationCellEditor(), DisplayMode.EDIT, null);
+		// Automatically open the editor for the cell below FIXME Make this configurable
+		configRegistry.registerConfigAttribute(EditConfigAttributes.OPEN_ADJACENT_EDITOR, true);
+		// Switch off out-of-the-box multi-cell editing
+		configRegistry.registerConfigAttribute(EditConfigAttributes.SUPPORT_MULTI_EDIT, false);
 	}
 
 	private IDisplayConverter getAnnotationValueConverter() {
@@ -52,37 +60,18 @@ public class GridEditConfiguration extends AbstractRegistryConfiguration {
 		};
 	}
 
-//	/**
-//		 * // TODO Add description
-//		 *
-//		 * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
-//		 * 
-//		 */
-//	public class AnnotationCellEditor extends TextCellEditor {
-//
-//		@Override
-//		public boolean commit(MoveDirectionEnum direction, boolean closeAfterCommit, boolean skipValidation) {
-//			if (!isClosed()) {
-//				// always do the conversion
-//				Object canonicalValue = getCanonicalValue();
-//				if (skipValidation || (!skipValidation && validateCanonicalValue(canonicalValue))) {
-//					boolean committed = new InlineEditHandler(layerCell.getLayer(), layerCell.getColumnPosition(),
-//							layerCell.getRowPosition()).commit(canonicalValue, direction);
-//					if (committed && closeAfterCommit) {
-//						close();
-//						if (direction != MoveDirectionEnum.NONE && openAdjacentEditor()) {
-//							this.layerCell.getLayer().doCommand(new EditSelectionCommand(null, this.configRegistry));
-//						}
-//					}
-//					return committed;
-//				}
-//				return false;
-//			}
-//			if (!isClosed()) {
-//				close();
-//			}
-//			return true;
-//		}
-//	}
+	/**
+		 * // TODO Add description
+		 *
+		 * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+		 * 
+		 */
+	public class AnnotationCellEditor extends TextCellEditor {
+
+		@Override
+		public boolean commit(MoveDirectionEnum direction, boolean closeAfterCommit, boolean skipValidation) {
+			return super.commit(MoveDirectionEnum.DOWN, true, true);
+		}
+	}
 
 }
