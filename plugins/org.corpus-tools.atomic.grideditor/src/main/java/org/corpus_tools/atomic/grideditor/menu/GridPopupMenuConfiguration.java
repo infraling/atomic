fbb28ac5e1d5c899196340edf1b504bd421306cb
Stephan Druskat <mail@sdruskat.net>
2017-06-02 11:38:38 +0200

    Add menu for creating new spans

--- plugins/org.corpus-tools.atomic.grideditor/src/main/java/org/corpus_tools/atomic/grideditor/menu/GridPopupMenuConfiguration.java
+++ plugins/org.corpus-tools.atomic.grideditor/src/main/java/org/corpus_tools/atomic/grideditor/menu/GridPopupMenuConfiguration.java
@@ -3,36 +3,24 @@
  */
 package org.corpus_tools.atomic.grideditor.menu;
 
-import java.util.HashSet;
-
 import org.corpus_tools.atomic.grideditor.data.annotationgrid.AnnotationGrid;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.config.AbstractUiBindingConfiguration;
-import org.eclipse.nebula.widgets.nattable.grid.GridRegion;
-import org.eclipse.nebula.widgets.nattable.grid.layer.GridLayer;
-import org.eclipse.nebula.widgets.nattable.layer.ILayer;
-import org.eclipse.nebula.widgets.nattable.layer.IUniqueIndexLayer;
-import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
 import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
-import org.eclipse.nebula.widgets.nattable.selection.command.SelectCellCommand;
-import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
-import org.eclipse.nebula.widgets.nattable.ui.action.IMouseAction;
+import org.eclipse.nebula.widgets.nattable.ui.NatEventData;
 import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
 import org.eclipse.nebula.widgets.nattable.ui.matcher.MouseEventMatcher;
 import org.eclipse.nebula.widgets.nattable.ui.menu.IMenuItemProvider;
-import org.eclipse.nebula.widgets.nattable.ui.menu.MenuItemProviders;
+import org.eclipse.nebula.widgets.nattable.ui.menu.IMenuItemState;
+import org.eclipse.nebula.widgets.nattable.ui.menu.PopupMenuAction;
 import org.eclipse.nebula.widgets.nattable.ui.menu.PopupMenuBuilder;
-import org.eclipse.nebula.widgets.nattable.viewport.ViewportLayer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.handlers.IHandlerService;
 
 /**
@@ -43,111 +31,141 @@ import org.eclipse.ui.handlers.IHandlerService;
  */
 public class GridPopupMenuConfiguration extends AbstractUiBindingConfiguration {
 
+	private static final String MENU_NEW_COLUMN = "New annotation column";
+	private static final String MENU_CREATE_SPAN = "Create span";
 	private final Menu menu;
-	private final AnnotationGrid annotationGrid;
-	private final NatTable natTable;
+	private final SelectionLayer selectionLayer;
+	private final AnnotationGrid grid;
 
-	public GridPopupMenuConfiguration(AnnotationGrid annotationGrid, NatTable natTable) {
-		this.menu = new PopupMenuBuilder(natTable).withMenuItemProvider("ColumnPopupMenu", new NewColumnMenuItemProvider()).build();
-		this.annotationGrid = annotationGrid;
-		this.natTable = natTable;
-	}
+    public GridPopupMenuConfiguration(final NatTable natTable, final AnnotationGrid grid, final SelectionLayer selectionLayer) {
+    	this.grid = grid;
+    	this.selectionLayer = selectionLayer;
+        this.menu = new PopupMenuBuilder(natTable)
+        		.withMenuItemProvider(MENU_NEW_COLUMN, new NewAnnotationColumnMenuItemProvider())
+                .withMenuItemProvider(MENU_CREATE_SPAN, new CreateSpanMenuItemProvider())
+                .withVisibleState(MENU_CREATE_SPAN, new MultiCellSelectionInOneColumnMenuItemState())
+                .build();
+    }
 
-	@Override
-	public void configureUiBindings(UiBindingRegistry uiBindingRegistry) {
-		uiBindingRegistry.registerMouseDownBinding(new MouseEventMatcher(SWT.NONE, GridRegion.BODY, MouseEventMatcher.RIGHT_BUTTON), new CellPopupMenuAction());
-	}
+    @Override
+    public void configureUiBindings(UiBindingRegistry uiBindingRegistry) {
+        uiBindingRegistry.registerMouseDownBinding(
+                new MouseEventMatcher(SWT.NONE, null, 3),
+                new PopupMenuAction(this.menu));
+    }
 
-	public class NewColumnMenuItemProvider implements IMenuItemProvider {
-
-		/*
-		 * (non-Javadoc)
+	/**
+		 * // TODO Add description
+		 *
+		 * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
 		 * 
-		 * @see org.eclipse.nebula.widgets.nattable.ui.menu.IMenuItemProvider#
-		 * addMenuItem(org.eclipse.nebula.widgets.nattable.NatTable,
-		 * org.eclipse.swt.widgets.Menu)
+		 */
+	public class NewAnnotationColumnMenuItemProvider implements IMenuItemProvider {
+	
+		/* (non-Javadoc)
+		 * @see org.eclipse.nebula.widgets.nattable.ui.menu.IMenuItemProvider#addMenuItem(org.eclipse.nebula.widgets.nattable.NatTable, org.eclipse.swt.widgets.Menu)
 		 */
 		@Override
 		public void addMenuItem(NatTable natTable, Menu popupMenu) {
-			MenuItem createNewColumnMenuItem = new MenuItem(popupMenu, SWT.PUSH);
-			createNewColumnMenuItem.setText("&New annotation column");
-			createNewColumnMenuItem.setEnabled(true);
-			createNewColumnMenuItem.addSelectionListener(new SelectionAdapter() {
+			MenuItem menuItem = new MenuItem(popupMenu, SWT.PUSH);
+            menuItem.setText("New annotation column");
+            menuItem.setEnabled(true);
+
+            menuItem.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent event) {
+                    executeCreateAnnotationColumn();
+                }
+
+				private void executeCreateAnnotationColumn() {
+					IHandlerService handlerService = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService(IHandlerService.class);
+					
+					Event event = new Event();
+					event.data = grid;
+					event.widget = natTable;
+					try {
+						handlerService.executeCommand("org.corpus_tools.atomic.grideditor.commands.newAnnotationColumn", event);
+					}
+					catch (Exception e1) {
+						throw new RuntimeException("Command org.corpus_tools.atomic.grideditor.commands.newAnnotationColumn not found!", e1);
+					}
+				}
+            });
+		}
+	}
+
+	/**
+		 * // TODO Add description
+		 *
+		 * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+		 * 
+		 */
+	public class CreateSpanMenuItemProvider implements IMenuItemProvider {
+	
+		/* (non-Javadoc)
+		 * @see org.eclipse.nebula.widgets.nattable.ui.menu.IMenuItemProvider#addMenuItem(org.eclipse.nebula.widgets.nattable.NatTable, org.eclipse.swt.widgets.Menu)
+		 */
+		@Override
+		public void addMenuItem(NatTable natTable, Menu popupMenu) {
+            MenuItem menuItem = new MenuItem(popupMenu, SWT.PUSH);
+            menuItem.setText("Create span");
+            menuItem.setEnabled(true);
+
+			menuItem.addSelectionListener(new SelectionAdapter() {
 				@Override
-				public void widgetSelected(SelectionEvent e) {
-					executeCreateColumnCommand();
+				public void widgetSelected(SelectionEvent event) {
+					executeCreateSpanCommand();
+				}
+
+				private void executeCreateSpanCommand() {
+					IHandlerService handlerService = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+							.getService(IHandlerService.class);
+
+					Event event = new Event();
+					event.data = grid;
+					event.widget = natTable;
+					try {
+						handlerService.executeCommand("org.corpus_tools.atomic.grideditor.commands.createSpan", event);
+					}
+					catch (Exception e1) {
+						throw new RuntimeException(
+								"Command org.corpus_tools.atomic.grideditor.commands.createSpan not found!", e1);
+					}
 				}
 			});
-			
-			/* 
-			 * TODO
-			 * Add new menu item for multi selection, in handler do
-			 * System.err.println(">>> " + ((HashSet<?>) ((StructuredSelection) HandlerUtil.getCurrentSelectionChecked(event)).getFirstElement()).size());
-			 * And check on size to see how to process multi selection
-			 * 
-			 * To do this for enabling only when multiple selection of cells, do:
-			 * ILayer underlyingLayer = natTable.getLayer();
-		if (underlyingLayer instanceof GridLayer) {
-			ILayer bodyLayer = ((GridLayer) underlyingLayer).getBodyLayer();
-			if (bodyLayer instanceof ViewportLayer) {
-				IUniqueIndexLayer scrollLayer = ((ViewportLayer) bodyLayer).getScrollableLayer();
-				if (scrollLayer instanceof SelectionLayer) {
-					selectionLayer = (SelectionLayer) scrollLayer;
-				}
-			 */
 		}
 
 	}
-	
+
 	/**
-	 * // TODO Add description
+	 * An {@link IMenuItemState} which is active only when
+	 * more than one cells in the {@link NatTable} are
+	 * selected, and all of the selected cells are in a
+	 * single column.
+	 *
+	 * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
 	 * 
 	 */
-	private void executeCreateColumnCommand() {
-		IHandlerService handlerService = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService(IHandlerService.class);
+	public class MultiCellSelectionInOneColumnMenuItemState implements IMenuItemState {
+
+		// FIXME: Also check for empty cells below, otherwise offer other span menu item
+		// Or disable completely when more than one annotaion value below
 		
-		Event event = new Event();
-		event.data = annotationGrid;
-		event.widget = natTable;
-		try {
-			handlerService.executeCommand("org.corpus_tools.atomic.grideditor.commands.newColumn", event);
-		}
-		catch (Exception e1) {
-			throw new RuntimeException("Command org.corpus_tools.atomic.grideditor.commands.newColumn not found!", e1);
-		}
-	}
-
-
-	/**
-	 * TODO Description
-	 *
-	 * @author Stephan Druskat <mail@sdruskat.net>
-	 *
-	 */
-	public class CellPopupMenuAction implements IMouseAction {
-
 		/*
 		 * (non-Javadoc)
 		 * 
 		 * @see
-		 * org.eclipse.nebula.widgets.nattable.ui.action.IMouseAction#run(org.
-		 * eclipse.nebula.widgets.nattable.NatTable,
-		 * org.eclipse.swt.events.MouseEvent)
+		 * org.eclipse.nebula.widgets.nattable.ui.menu.IMenuItemState#isActive(
+		 * org.eclipse.nebula.widgets.nattable.ui.NatEventData)
 		 */
 		@Override
-		public void run(NatTable natTable, MouseEvent event) {
-			int columnPosition = natTable.getColumnPositionByX(event.x);
-			int rowPosition = natTable.getRowPositionByY(event.y);
-
-			ILayerCell cell = natTable.getCellByPosition(columnPosition, rowPosition);
-
-			if (!cell.getDisplayMode().equals(DisplayMode.SELECT)) {
-				natTable.doCommand(new SelectCellCommand(natTable, columnPosition, rowPosition, false, false));
-			}
-
-			menu.setData(MenuItemProviders.NAT_EVENT_DATA_KEY, event.data);
-			menu.setVisible(true);
+		public boolean isActive(NatEventData natEventData) {
+			int noOfSelCells = selectionLayer.getSelectedCells().size();
+			int noOfSelCols = selectionLayer.getSelectedColumnPositions().length;
+			return noOfSelCells > 1 && noOfSelCols == 1;
 		}
 
 	}
+
 }
+//}
