57861ecae224a6a78a6988e0db595e81a7c70763
Stephan Druskat <mail@sdruskat.net>
2017-06-09 14:32:16 +0200

    Add util class and method for calculating indices for spanned NatTable cells

---
+++ plugins/org.corpus-tools.atomic.grideditor/src/main/java/org/corpus_tools/atomic/grideditor/utils/CellUtils.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.corpus_tools.atomic.grideditor.utils;
+
+import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
+
+import com.google.common.collect.Range;
+
+/**
+ * // TODO Add description
+ *
+ * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+ * 
+ */
+public class CellUtils {
+	
+	public static Range<Integer> getRowIndicesForCell(ILayerCell cell) {
+		if (cell.isSpannedCell()) {
+			int startRowPositition = cell.getOriginRowPosition();
+			int rowPosition = cell.getRowPosition();
+			int distanceThisRowStartRow = rowPosition - startRowPositition;
+			int rowIndex = cell.getRowIndex();
+			int startIndex = rowIndex - distanceThisRowStartRow;
+			return Range.closedOpen(startIndex, startIndex + cell.getRowSpan() - 1);
+		}
+		else {
+			return Range.closed(cell.getRowIndex(), cell.getRowIndex());
+		}
+	}
+
+}
---
+++ plugins/org.corpus-tools.atomic.grideditor/src/test/java/org/corpus_tools/atomic/grideditor/utils/CellUtilsTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.corpus_tools.atomic.grideditor.utils;
+
+import static org.junit.Assert.*; 
+
+import org.junit.Test;
+//import static org.mockito.Mockito.*;
+
+/**
+ * // TODO Add description
+ *
+ * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+ * 
+ */
+public class CellUtilsTest {
+
+	/**
+	 * Test method for {@link org.corpus_tools.atomic.grideditor.utils.CellUtils#getRowIndicesForCell(org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell)}.
+	 */
+	@Test
+	public void testGetRowIndicesForSingleCell() {
+		// FIME Mock cell
+//		assertEquals(3, actual);
+	}
+
+}
