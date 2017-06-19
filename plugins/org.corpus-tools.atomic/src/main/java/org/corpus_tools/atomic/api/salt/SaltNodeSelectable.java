fe7ed9e032d8417cf9e17af29b6515df21a46400
Stephan Druskat <mail@sdruskat.net>
2017-06-19 13:24:48 +0200

    Clean up

--- plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/api/salt/SaltNodeSelectable.java
+++ plugins/org.corpus-tools.atomic/src/main/java/org/corpus_tools/atomic/api/salt/SaltNodeSelectable.java
@@ -2,8 +2,6 @@ package org.corpus_tools.atomic.api.salt;
 
 import java.util.List;
 
-import org.eclipse.emf.common.util.URI;
-
 /**
  * Interface for all components where an external party can select a number of salt nodes by their name.
  * @author Thomas Krause <krauseto@hu-berlin.de>
