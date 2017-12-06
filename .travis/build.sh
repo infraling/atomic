b5366833a7624212d4bd3b0d0c41e3db7a49446e
Travis CI <travis@travis-ci.org>
2017-12-06 19:01:44 +0100

    Try another Travis build with push

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+maven_build() {
+	mvn clean package
+}
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
+commit_files() {
   git checkout -b travis-build
-  git -f add .
+  git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://${GITHUB_TOKEN}@github.com/infraling/atomic.git > /dev/null 2>&1
-  git push -u origin travis-build
+  git remote add origin-travis https://${GITHUB_TOKEN}@github.com/infraling/atomic.git
+  git push --quiet --set-upstream origin-travis travis-build
 }
 
+maven_build
 setup_git
-commit_website_files
+commit_files
 upload_files
